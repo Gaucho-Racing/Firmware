a902ad2213099086362af82d3e9110ca95cb31c5
Daniel Hansen <dchansen06@gmail.com>
2025-08-02 21:43:53 -0700

    Correct link flags to use general path and not Will's computer

--- Lib/Platform/STM32G474/stm32g474.cmake
+++ Lib/Platform/STM32G474/stm32g474.cmake
@@ -8,8 +8,8 @@ add_library(${CHIP}_LIB INTERFACE)
 enable_language(C ASM)
 
 target_compile_definitions(${CHIP}_LIB INTERFACE 
-	USE_FULL_LL_DRIVER 
-	USE_HAL_DRIVER 
+	USE_FULL_LL_DRIVER
+	USE_HAL_DRIVER
 	${CHIP}xx
 )
 
@@ -110,6 +110,7 @@ target_sources(${CHIP}_LIB INTERFACE
 )
 
 function(add_executable_stmg474re TARGET_NAME)
+
 set(TARGET_FLAGS "-mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard")
 
 add_executable(${TARGET_NAME})
