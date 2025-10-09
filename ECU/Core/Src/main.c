c9af0160408dd50819a86e3d94737e7562e3c0f5
Daniel Hansen <dchansen06@gmail.com>
2025-10-09 03:01:48 -0700

    Undo formatting test

--- ECU/Core/Src/main.c
+++ ECU/Core/Src/main.c
@@ -114,9 +114,7 @@ int main(void)
 	/* USER CODE BEGIN WHILE */
 	while (1) {
 		/* USER CODE END WHILE */
-		if (NULL) {
-			(void)(NULL);
-		}
+
 		/* USER CODE BEGIN 3 */
 	}
 	/* USER CODE END 3 */
