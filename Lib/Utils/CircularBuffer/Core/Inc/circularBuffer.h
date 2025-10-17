c8b9f7203aec87a764ed3372863487026a15b1ec
Thomas Xu <thomasxu@ucsb.edu>
2025-10-16 21:46:35 -0700

    Added comments; minor bug fix

--- Lib/Utils/CircularBuffer/Core/Inc/circularBuffer.h
+++ Lib/Utils/CircularBuffer/Core/Inc/circularBuffer.h
@@ -3,29 +3,33 @@
 
 #include <stdint.h>
 
-
+// Define the circularBuffer data type
 typedef struct{
-	uint32_t head;
-	uint32_t tail; // exclusive
-	uint32_t size;
-	void** buffer;
+	uint32_t head; // the head position inclusive
+	uint32_t tail; // the tail position exclusive
+	uint32_t size; // the number of items in buffer
+	void** buffer; // the buffer body
 
-	// void (*circularBufferPush)(void* object);
-	// TODO: generate hash with all attributes to reinforce data integrety
+	// Idea: generate hash with all attributes to reinforce data integrety
 } circularBuffer;
 
+// Define the holder of circularBuffer functions, a static instance of this
+// is initialized at the end of this file
 struct circularBufferFuncs{
 	void(*push)(circularBuffer, void*);
 	void*(*pop)(circularBuffer);
 	void*(*peek)(circularBuffer);
 };
 
+// Buffer manipulation functions defined
 void push(circularBuffer buffer, void* object);
 void* pop(circularBuffer buffer);
 void* peek(circularBuffer buffer);
+// Buffer initialization/deinitialization functions defined
 circularBuffer createCircularBuffer(uint32_t size);
 void freeCircularBuffer(circularBuffer buffer);
 
+// Static instance of functions' container
 static struct circularBufferFuncs circularBufferFuncs = {push, pop, peek};
 
 #endif
--- Lib/Utils/CircularBuffer/Core/Src/circularBuffer.c
+++ Lib/Utils/CircularBuffer/Core/Src/circularBuffer.c
@@ -1,38 +1,47 @@
 #include "circularBuffer.h"
-#include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 #include <string.h>
 
 void push(circularBuffer buffer, void* object){
+	// Remove the buffer head if it's going to be overwritten
 	if(buffer.head == buffer.tail){
 		free(buffer.buffer[buffer.head]);
 		buffer.head++;
 		if(buffer.head == buffer.size)
 			buffer.head = 0;
 	}
+	// Copy object contents to the buffer
+	buffer.buffer[buffer.tail] = malloc(sizeof(*object));
 	memcpy(buffer.buffer[buffer.tail], object, sizeof(*object));
+	// Update tail iterator
 	buffer.tail++;
 	if(buffer.tail == buffer.size)
 		buffer.tail = 0;
 }
 
 void* pop(circularBuffer buffer){
+	// Copy buffer head to result
 	void* result = malloc(sizeof(*buffer.buffer[buffer.head]));
 	memcpy(result, buffer.buffer[buffer.head], sizeof(*buffer.buffer[buffer.head]));
+	// Remove buffer head
 	free(buffer.buffer[buffer.head]);
+	// Update head iterator
 	buffer.head++;
 	if(buffer.head == buffer.size)
 		buffer.head = 0;
+	// Return result
 	return result;
 }
 
 void* peek(circularBuffer buffer){
+	// Copy buffer head to result and return it
 	void* result = malloc(sizeof(*buffer.buffer[buffer.head]));
 	memcpy(result, buffer.buffer[buffer.head], sizeof(*buffer.buffer[buffer.head]));
 	return result;
 }
 
 circularBuffer createCircularBuffer(uint32_t size){
+	// For specifications see the header file
 	circularBuffer buffer;
 	buffer.head = 0;
 	buffer.tail = 0;
