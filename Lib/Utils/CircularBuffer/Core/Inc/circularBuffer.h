a9f7dcfc98664369251029e7d48b8fe907d416a8
Thomas Xu <thomasxu@ucsb.edu>
2025-10-19 19:01:38 -0700

    fix issue: removed calls to sizeof(*(void*)), which always returns 1.

--- Lib/Utils/CircularBuffer/Core/Inc/circularBuffer.h
+++ Lib/Utils/CircularBuffer/Core/Inc/circularBuffer.h
@@ -1,6 +1,7 @@
 #ifndef _CIRCULAR_BUFFER_H_
 #define _CIRCULAR_BUFFER_H_
 
+#include <stddef.h>
 #include <stdint.h>
 
 // Define the new data type CircularBuffer
@@ -8,9 +9,9 @@ typedef struct circular_buffer_st CircularBuffer;
 
 // Define the wrapper for CircularBuffer functions
 struct circular_buffer_ops_st{
-	CircularBuffer*(* const create)(uint32_t size);
+	CircularBuffer*(* const create)(uint32_t capacity);
 	void(* const free)(CircularBuffer* buffer);
-	void(* const push)(CircularBuffer* buffer, void* object);
+	void(* const push)(CircularBuffer* buffer, void* object, size_t size);
 	void*(* const pop)(CircularBuffer* buffer);
 	void*(* const peek)(CircularBuffer* buffer);
 };
--- Lib/Utils/CircularBuffer/Core/Src/circularBuffer.c
+++ Lib/Utils/CircularBuffer/Core/Src/circularBuffer.c
@@ -1,32 +1,74 @@
 #include "circularBuffer.h"
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+
+// Define the Item type, a wrapper of stored items
+typedef struct{
+	void* ptr;   // pointer to the item
+	size_t size; // size of the item
+} Item;
 
 // Define the circularBuffer data type
 struct circular_buffer_st{
 	uint32_t head; // the head position inclusive
 	uint32_t tail; // the tail position exclusive
-	uint32_t size; // the number of items in buffer
-	void** buffer; // the buffer body
+	uint32_t capacity; // the number of items in buffer
+	Item** buffer; // the buffer body
 
 	// Idea: generate hash with all attributes to reinforce data integrety
 };
 
-void cb_push(CircularBuffer* buffer, void* object){
+CircularBuffer* cb_create(uint32_t capacity){
+	// Return null pointer if an invalid size(< 1) is specified
+	if(capacity < 1)
+		return NULL;
+	// For specifications see the header file
+	CircularBuffer* buffer = malloc(sizeof(CircularBuffer));
+	buffer->head = 0;
+	buffer->tail = 0;
+	buffer->capacity = capacity;
+	buffer->buffer = malloc(capacity*sizeof(Item*));
+	return buffer;
+}
+
+void cb_free(CircularBuffer* buffer){
+	// Error check
+	if(buffer == NULL)
+		return;
+	// Free contents
+	for(uint32_t i = 0; i < buffer->capacity; i++){
+		if(buffer->buffer[i] != NULL){
+			// Free item body
+			free(buffer->buffer[i]->ptr); // TODO: Recursively free stored items if a method becomes available
+			// Free item wrapper
+			free(buffer->buffer[i]);
+		}
+	}
+	// Free buffer body
+	free(buffer->buffer);
+	// Free buffer instance
+	free(buffer);
+}
+
+void cb_push(CircularBuffer* buffer, void* object, size_t size){
 	// Remove the buffer head if it's going to be overwritten
 	// That is, if the buffer is already full
 	if(buffer->buffer[buffer->head] != NULL && buffer->head == buffer->tail){
+		free(buffer->buffer[buffer->head]->ptr); // TODO: Make deeper free; see cb_free.
 		free(buffer->buffer[buffer->head]);
 		buffer->head++;
-		if(buffer->head == buffer->size)
+		if(buffer->head == buffer->capacity)
 			buffer->head = 0;
 	}
 	// Copy object contents to the buffer
-	buffer->buffer[buffer->tail] = malloc(sizeof(*object));
-	memcpy(buffer->buffer[buffer->tail], object, sizeof(*object));
+	buffer->buffer[buffer->tail] = malloc(sizeof(Item)); // allocate wrapper memory
+	buffer->buffer[buffer->tail]->ptr = malloc(size); // allocate object memory
+	buffer->buffer[buffer->tail]->size = size; // record item size
+	memcpy(buffer->buffer[buffer->tail]->ptr, object, size);
 	// Update tail iterator
 	buffer->tail++;
-	if(buffer->tail == buffer->size)
+	if(buffer->tail == buffer->capacity)
 		buffer->tail = 0;
 }
 
@@ -35,14 +77,15 @@ void* cb_pop(CircularBuffer* buffer){
 	if(buffer->buffer[buffer->head] == NULL)
 		return NULL;
 	// Copy buffer head to result
-	void* result = malloc(sizeof(*buffer->buffer[buffer->head]));
-	memcpy(result, buffer->buffer[buffer->head], sizeof(*buffer->buffer[buffer->head]));
+	void* result = malloc(buffer->buffer[buffer->head]->size);
+	memcpy(result, buffer->buffer[buffer->head], buffer->buffer[buffer->head]->size);
 	// Remove buffer head
+	free(buffer->buffer[buffer->head]->ptr); // TODO: Make deeper free; see cb_free.
 	free(buffer->buffer[buffer->head]);
 	buffer->buffer[buffer->head] = NULL;
 	// Update head iterator
 	buffer->head++;
-	if(buffer->head == buffer->size)
+	if(buffer->head == buffer->capacity)
 		buffer->head = 0;
 	// Return result
 	return result;
@@ -53,35 +96,9 @@ void* cb_peek(CircularBuffer* buffer){
 	if(buffer->buffer[buffer->head] == NULL)
 		return NULL;
 	// Copy buffer head to result and return it
-	void* result = malloc(sizeof(*buffer->buffer[buffer->head]));
-	memcpy(result, buffer->buffer[buffer->head], sizeof(*buffer->buffer[buffer->head]));
+	void* result = malloc(buffer->buffer[buffer->head]->size);
+	memcpy(result, buffer->buffer[buffer->head], buffer->buffer[buffer->head]->size);
 	return result;
-}
-
-CircularBuffer* cb_create(uint32_t size){
-	// Return null pointer if an invalid size(< 1) is specified
-	if(size < 1)
-		return NULL;
-	// For specifications see the header file
-	CircularBuffer* buffer = malloc(sizeof(CircularBuffer));
-	buffer->head = 0;
-	buffer->tail = 0;
-	buffer->size = size;
-	buffer->buffer = malloc(size*sizeof(void*));
-	return buffer;
-}
-
-void cb_free(CircularBuffer* buffer){
-	// Error check
-	if(buffer == NULL)
-		return;
-	// Free contents
-	for(uint32_t i = 0; i < buffer->size; i++)
-		free(buffer->buffer[i]);
-	// Free buffer body
-	free(buffer->buffer);
-	// Free buffer instance
-	free(buffer);
 }
 
 const struct circular_buffer_ops_st cbOps = {cb_create, cb_free, cb_push, cb_pop, cb_peek};
