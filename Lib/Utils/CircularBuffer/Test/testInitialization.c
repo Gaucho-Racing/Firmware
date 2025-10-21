cee40c773bb6629c44851049d753e0426516e879
Thomas Xu <thomasxu@ucsb.edu>
2025-10-21 02:25:15 -0700

    splitted tests into multiple files

---
+++ Lib/Utils/CircularBuffer/Test/testInitialization.c
@@ -0,0 +1,26 @@
+#include "circularBuffer.h"
+
+int main()
+{
+	CircularBuffer *tmp;
+
+	tmp = GR_CircularBuffer_Create(0);
+	if (tmp != NULL) {
+		GR_CircularBuffer_Free(tmp);
+		return 1;
+	}
+
+	tmp = GR_CircularBuffer_Create(5);
+	if (tmp == NULL) {
+		return 2;
+	}
+	GR_CircularBuffer_Free(tmp);
+
+	tmp = GR_CircularBuffer_Create(100);
+	if (tmp == NULL) {
+		return 3;
+	}
+	GR_CircularBuffer_Free(tmp);
+
+	return 0;
+}
---
+++ Lib/Utils/CircularBuffer/Test/testPushPop.c
@@ -0,0 +1,53 @@
+#include "circularBuffer.h"
+
+int main()
+{
+
+	CircularBuffer *buffer = GR_CircularBuffer_Create(10);
+
+	// pushing without overwriting
+	for (int i = 0; i < 10; i++) {
+		int tmp = i;
+		GR_CircularBuffer_Push(buffer, &tmp, sizeof(tmp));
+	}
+	for (int i = 0; i < 10; i++) {
+		int *tmp;
+		tmp = GR_CircularBuffer_Pop(buffer);
+		if (*tmp != i) {
+			GR_CircularBuffer_Free(buffer);
+			return 1;
+		}
+	}
+
+	// poping empty buffer
+	for (int i = 0; i < 1000; i++) {
+		GR_CircularBuffer_Pop(buffer);
+	}
+	for (int i = 0; i < 1000; i++) {
+		if (GR_CircularBuffer_Pop(buffer) != NULL) { // Should do nothing
+			GR_CircularBuffer_Free(buffer);
+			return 2;
+		}
+	}
+
+	// pushing past size limite
+	for (int i = 0; i < 1000; i++) {
+		int tmp = i;
+		GR_CircularBuffer_Push(buffer, &tmp, sizeof(tmp));
+	}
+	for (int i = 0; i < 10; i++) {
+		int tmp = i;
+		GR_CircularBuffer_Push(buffer, &tmp, sizeof(tmp));
+	}
+	for (int i = 0; i < 10; i++) {
+		int *tmp;
+		tmp = GR_CircularBuffer_Pop(buffer);
+		if (*tmp != i) {
+			GR_CircularBuffer_Free(buffer);
+			return 3;
+		}
+	}
+
+	GR_CircularBuffer_Free(buffer);
+	return 0;
+}
