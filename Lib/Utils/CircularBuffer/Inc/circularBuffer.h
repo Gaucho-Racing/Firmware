bbae35c80ceda0a5dd9c94ce5c8eeb5651ef47a5
Thomas Xu <thomas.xu.7@outlook.com>
2025-10-23 15:10:59 -0700

    revise code to improve readability; added the inspection functions

--- Lib/Utils/CircularBuffer/Inc/circularBuffer.h
+++ Lib/Utils/CircularBuffer/Inc/circularBuffer.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 // Define the new data type CircularBuffer
 typedef struct circular_buffer_st CircularBuffer;
@@ -18,6 +19,26 @@ CircularBuffer *GR_CircularBuffer_Create(uint32_t capacity);
 ///        freed by this function.
 /// @param buffer The pointer to the CircularBuffer to free.
 void GR_CircularBuffer_Free(CircularBuffer *buffer);
+
+/// @brief Get a CircularBuffer's maximum capacity.
+/// @param buffer The pointer to the CircularBuffer to inspect.
+/// @return The buffer's capacity, in number of items.
+uint32_t GR_CircularBuffer_GetCapacity(CircularBuffer *buffer);
+
+/// @brief Get a CircularBuffer's currently used size.
+/// @param buffer The pointer to the CircularBuffer to inspect.
+/// @return The buffer's usage, in number of items stored.
+uint32_t GR_CircularBuffer_GetCurrentSize(CircularBuffer *buffer);
+
+/// @brief Check if a CircularBuffer is full.
+/// @param buffer The pointer to the CircularBuffer to inspect.
+/// @return true if full, false otherwise.
+bool GR_CircularBuffer_IsFull(CircularBuffer *buffer);
+
+/// @brief Check if a CircularBuffer is empty.
+/// @param buffer The pointer to the CircularBuffer to inspect.
+/// @return true if empty, false otherwise.
+bool GR_CircularBuffer_IsEmpty(CircularBuffer *buffer);
 
 /// @brief Push a copy of an item into a circular buffer.
 /// @param buffer Pointer to the CircularBuffer to store the item.
