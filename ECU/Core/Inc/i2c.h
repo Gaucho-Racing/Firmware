4a183bcc531b530b8e1bf65a633f95eb6e918978
Ivy Holiday <iholiday@ucsb.edu>
2025-10-26 21:55:30 -0700

    added basic framework for i2c_interface.h and i2c_interface.c, fixed some include issues by updating i2c.h

--- ECU/Core/Inc/i2c.h
+++ ECU/Core/Inc/i2c.h
@@ -29,7 +29,7 @@ extern "C" {
 #include "main.h"
 
 /* USER CODE BEGIN Includes */
-
+#include "stm32g4xx_hal_i2c.h"
 /* USER CODE END Includes */
 
 extern I2C_HandleTypeDef hi2c1;
---
+++ ECU/Core/Src/i2c_interface.c
@@ -0,0 +1,11 @@
+#include "i2c.h"
+#include "i2c_interface.h"
+
+#define I2C_TIMEOUT 100 // Timeout duration
+
+HAL_StatusTypeDef I2C_Write(uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout) {
+    return HAL_I2C_Master_Transmit(&hi2c1, DevAddress, pData, Size, Timeout);
+}
+HAL_StatusTypeDef I2C_Read(uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout) {
+    return HAL_I2C_Master_Receive(&hi2c1, DevAddress, pData, Size, Timeout);
+}
