8d3a8767b4808f7dc27aa012b9b53ada94dd4361
Anthony Ma <anthony497@ucsb.edu>
2025-10-27 19:49:30 -0700

    Changed test get capacity to work with the new version of circular buffer

--- Lib/Utils/CircularBuffer/Test/testGetCapacity.c
+++ Lib/Utils/CircularBuffer/Test/testGetCapacity.c
@@ -6,9 +6,27 @@ int main()
 	CircularBuffer *buffer;
 
 	// Test GetCapacity
-	buffer = GR_CircularBuffer_Create(10);
+	buffer = GR_CircularBuffer_Create(10, CIRCULAR_BUFFER_FREE_ITEM_ON_OVERWRITE);
 	if (GR_CircularBuffer_GetCapacity(buffer) != 10) {
 		return 1;
+	}
+	GR_CircularBuffer_Free(buffer);
+
+	buffer = GR_CircularBuffer_Create(10, CIRCULAR_BUFFER_KEEP_ITEM_ON_OVERWRITE);
+	if (GR_CircularBuffer_GetCapacity(buffer) != 10) {
+		return 2;
+	}
+	GR_CircularBuffer_Free(buffer);
+
+	buffer = GR_CircularBuffer_Create(25, CIRCULAR_BUFFER_FREE_ITEM_ON_OVERWRITE);
+	if (GR_CircularBuffer_GetCapacity(buffer) != 25) {
+		return 3;
+	}
+	GR_CircularBuffer_Free(buffer);
+
+	buffer = GR_CircularBuffer_Create(25, CIRCULAR_BUFFER_KEEP_ITEM_ON_OVERWRITE);
+	if (GR_CircularBuffer_GetCapacity(buffer) != 25) {
+		return 4;
 	}
 	GR_CircularBuffer_Free(buffer);
 
