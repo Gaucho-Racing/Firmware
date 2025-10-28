0cb60f05da91e611d2db68936058673685c5708f
Anthony Ma <anthony497@ucsb.edu>
2025-10-27 19:47:26 -0700

    Changed test initialization for the new version of Circular Buffer

--- Lib/Utils/CircularBuffer/Test/testInitialization.c
+++ Lib/Utils/CircularBuffer/Test/testInitialization.c
@@ -5,21 +5,27 @@ int main()
 {
 	CircularBuffer *tmp;
 
-	tmp = GR_CircularBuffer_Create(0);
+	tmp = GR_CircularBuffer_Create(0, CIRCULAR_BUFFER_KEEP_ITEM_ON_OVERWRITE);
 	if (tmp != NULL) {
 		GR_CircularBuffer_Free(tmp);
 		return 1;
 	}
 
-	tmp = GR_CircularBuffer_Create(5);
-	if (tmp == NULL) {
+	tmp = GR_CircularBuffer_Create(0, CIRCULAR_BUFFER_FREE_ITEM_ON_OVERWRITE);
+	if (tmp != NULL) {
+		GR_CircularBuffer_Free(tmp);
 		return 2;
+	}
+
+	tmp = GR_CircularBuffer_Create(5, CIRCULAR_BUFFER_KEEP_ITEM_ON_OVERWRITE);
+	if (tmp == NULL) {
+		return 3;
 	}
 	GR_CircularBuffer_Free(tmp);
 
-	tmp = GR_CircularBuffer_Create(100);
+	tmp = GR_CircularBuffer_Create(5, CIRCULAR_BUFFER_FREE_ITEM_ON_OVERWRITE);
 	if (tmp == NULL) {
-		return 3;
+		return 4;
 	}
 	GR_CircularBuffer_Free(tmp);
 
