c2aaeba1a1fec6b7942c9d8833262bdff1ebe69b
Anthony Ma <anthony497@ucsb.edu>
2025-10-27 20:01:37 -0700

    Changed test current size to work with the new version of circular buffer

--- Lib/Utils/CircularBuffer/Test/testGetCurrentSize.c
+++ Lib/Utils/CircularBuffer/Test/testGetCurrentSize.c
@@ -6,58 +6,113 @@ int main()
 	CircularBuffer *buffer;
 
 	// Test GetCurrentSize
-	buffer = GR_CircularBuffer_Create(10);
+	buffer = GR_CircularBuffer_Create(10, CIRCULAR_BUFFER_KEEP_ITEM_ON_OVERWRITE);
 	// Test single item
 	int temp = 0;
-	GR_CircularBuffer_Push(buffer, &temp, sizeof(temp));
+	GR_CircularBuffer_Push(buffer, &temp);
 	if (GR_CircularBuffer_GetCurrentSize(buffer) != 1) {
 		return 1;
 	}
 	GR_CircularBuffer_Free(buffer);
 
-	// Push multiple items
-	buffer = GR_CircularBuffer_Create(10);
-	for (int i = 0; i < 6; i++) {
-		GR_CircularBuffer_Push(buffer, &i, sizeof(temp));
-	}
-	if (GR_CircularBuffer_GetCurrentSize(buffer) != 6) {
+	buffer = GR_CircularBuffer_Create(10, CIRCULAR_BUFFER_FREE_ITEM_ON_OVERWRITE);
+	GR_CircularBuffer_Push(buffer, &temp);
+	if (GR_CircularBuffer_GetCurrentSize(buffer) != 1) {
 		return 2;
 	}
 	GR_CircularBuffer_Free(buffer);
 
-	// Push over limit
-	buffer = GR_CircularBuffer_Create(10);
-	for (int i = 0; i < 12; i++) {
-		GR_CircularBuffer_Push(buffer, &i, sizeof(temp));
+	// Push multiple items
+	buffer = GR_CircularBuffer_Create(10, CIRCULAR_BUFFER_KEEP_ITEM_ON_OVERWRITE);
+	int arr1[6] = {0};
+	for (int i = 0; i < 6; i++) {
+		GR_CircularBuffer_Push(buffer, &arr1[i]);
 	}
-	if (GR_CircularBuffer_GetCurrentSize(buffer) != 10) {
+	if (GR_CircularBuffer_GetCurrentSize(buffer) != 6) {
 		return 3;
 	}
 	GR_CircularBuffer_Free(buffer);
 
+	buffer = GR_CircularBuffer_Create(10, CIRCULAR_BUFFER_FREE_ITEM_ON_OVERWRITE);
+	for (int i = 0; i < 6; i++) {
+		GR_CircularBuffer_Push(buffer, &arr1[i]);
+	}
+	if (GR_CircularBuffer_GetCurrentSize(buffer) != 6) {
+		return 4;
+	}
+	GR_CircularBuffer_Free(buffer);
+
+	// Push over limit
+	buffer = GR_CircularBuffer_Create(10, CIRCULAR_BUFFER_FREE_ITEM_ON_OVERWRITE);
+	int arr2[12] = {0};
+	for (int i = 0; i < 12; i++) {
+		GR_CircularBuffer_Push(buffer, &arr2[i]);
+	}
+	if (GR_CircularBuffer_GetCurrentSize(buffer) != 10) {
+		return 5;
+	}
+	GR_CircularBuffer_Free(buffer);
+	
+	buffer = GR_CircularBuffer_Create(10, CIRCULAR_BUFFER_KEEP_ITEM_ON_OVERWRITE);
+	for (int i = 0; i < 12; i++) {
+		GR_CircularBuffer_Push(buffer, &arr2[i]);
+	}
+	if (GR_CircularBuffer_GetCurrentSize(buffer) != 10) {
+		return 6;
+	}
+	GR_CircularBuffer_Free(buffer);
+
 	// Push and pop
-	buffer = GR_CircularBuffer_Create(10);
+	buffer = GR_CircularBuffer_Create(10, CIRCULAR_BUFFER_FREE_ITEM_ON_OVERWRITE);
 	for (int i = 0; i < 9; i++) {
-		GR_CircularBuffer_Push(buffer, &i, sizeof(temp));
+		GR_CircularBuffer_Push(buffer, &arr2[i]);
 	}
 	for (int i = 0; i < 4; i++) {
 		GR_CircularBuffer_Pop(buffer);
 	}
 	if (GR_CircularBuffer_GetCurrentSize(buffer) != 5) {
-		return 4;
+		return 7;
+	}
+	GR_CircularBuffer_Free(buffer);
+
+	
+	buffer = GR_CircularBuffer_Create(10, CIRCULAR_BUFFER_KEEP_ITEM_ON_OVERWRITE);
+	for (int i = 0; i < 9; i++) {
+		GR_CircularBuffer_Push(buffer, &arr2[i]);
+	}
+	for (int i = 0; i < 4; i++) {
+		GR_CircularBuffer_Pop(buffer);
+	}
+	if (GR_CircularBuffer_GetCurrentSize(buffer) != 5) {
+		return 8;
 	}
 	GR_CircularBuffer_Free(buffer);
 
 	// Push over limit and pop
-	buffer = GR_CircularBuffer_Create(10);
+	buffer = GR_CircularBuffer_Create(10, CIRCULAR_BUFFER_KEEP_ITEM_ON_OVERWRITE);
+	int arr3[15] = {0};
 	for (int i = 0; i < 15; i++) {
-		GR_CircularBuffer_Push(buffer, &i, sizeof(temp));
+		GR_CircularBuffer_Push(buffer, &arr3[i]);
 	}
 	for (int i = 0; i < 4; i++) {
 		GR_CircularBuffer_Pop(buffer);
 	}
 	if (GR_CircularBuffer_GetCurrentSize(buffer) != 6) {
-		return 5;
+		return 9;
+	}
+	GR_CircularBuffer_Free(buffer);
+
+	
+	buffer = GR_CircularBuffer_Create(10, CIRCULAR_BUFFER_FREE_ITEM_ON_OVERWRITE);
+	int arr3[15] = {0};
+	for (int i = 0; i < 15; i++) {
+		GR_CircularBuffer_Push(buffer, &arr3[i]);
+	}
+	for (int i = 0; i < 4; i++) {
+		GR_CircularBuffer_Pop(buffer);
+	}
+	if (GR_CircularBuffer_GetCurrentSize(buffer) != 6) {
+		return 10;
 	}
 	GR_CircularBuffer_Free(buffer);
 
