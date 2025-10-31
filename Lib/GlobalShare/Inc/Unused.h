0355fe95ad67ca738a6260a7b64f674fc97df282
Daniel Hansen <dchansen06@gmail.com>
2025-10-31 03:10:40 -0700

    Add UNUSED macro to mark unused variables and parameters

---
+++ Lib/GlobalShare/Inc/Unused.h
@@ -0,0 +1,13 @@
+#ifndef _UNUSED_H_
+    #define _UNUSED_H_
+
+    /**
+     * @brief Macro to mark a variable or parameter as unused.
+     *
+     * This macro helps to avoid compiler warnings about unused variables or parameters.
+     * It casts the variable to void, indicating that it is intentionally unused.
+     *
+     * @param x The variable or parameter to be marked as unused.
+     */
+    #define UNUSED(x) (void)(x)
+#endif
