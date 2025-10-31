dc718b35a4e46678a61ef340fe594955bb2cb672
Daniel Hansen <dchansen06@gmail.com>
2025-10-31 03:21:15 -0700

    Refactor CMake configuration for logomatic executables to improve readability and organization

--- Lib/GlobalShare/common.cmake
+++ Lib/GlobalShare/common.cmake
@@ -23,15 +23,25 @@ endif()
 if(CMAKE_BUILD_TYPE STREQUAL "Test")
 	target_compile_definitions(GLOBALSHARE_LIB INTERFACE LOGOMATIC_ENABLED)
 
-	add_executable(
-		logomatic_simple
+	add_executable(logomatic_simple)
+	target_include_directories(
+		logomatic_simple PRIVATE
+		${CMAKE_CURRENT_LIST_DIR}/Inc
+	)
+	target_sources(
+		logomatic_simple PRIVATE
 		${CMAKE_CURRENT_LIST_DIR}/Test/logomatic_simple_print.c
 	)
 	target_link_libraries(logomatic_simple PRIVATE GLOBALSHARE_LIB)
 	add_test(logomatic_simple_test logomatic_simple)
 
-	add_executable(
-		logomatic_float
+	add_executable(logomatic_float)
+	target_include_directories(
+		logomatic_float PRIVATE
+		${CMAKE_CURRENT_LIST_DIR}/Inc
+	)
+	target_sources(
+		logomatic_float PRIVATE
 		${CMAKE_CURRENT_LIST_DIR}/Test/logomatic_float_print.c
 	)
 	target_link_libraries(logomatic_float PRIVATE GLOBALSHARE_LIB)
