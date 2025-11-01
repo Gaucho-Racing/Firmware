ef9e6c8fd3f5289afc7012493c13253a1a393bf2
Daniel Hansen <dchansen06@gmail.com>
2025-11-01 16:47:10 -0700

    Intentional memory leak

--- Lib/GlobalShare/Test/logomatic_float_print.c
+++ Lib/GlobalShare/Test/logomatic_float_print.c
@@ -1,9 +1,11 @@
 #include <math.h>
+#include <stdlib.h>
 
 #include "Logomatic.h"
 
 int main(void)
 {
 	LOGOMATIC("I like %f\n", M_PI);
+	malloc(42);	// Intentional memory leak for testing
 	return 0;
 }
