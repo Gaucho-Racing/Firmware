9e9b1b7ba30a3a59fe46da7b56c43436b524c80a
SlowThomas <thomas.xu.7@outlook.com>
2025-11-01 00:31:30 -0700

    improve vairable naming

--- Lib/Utils/CircularBuffer/Inc/circularBuffer.h
+++ Lib/Utils/CircularBuffer/Inc/circularBuffer.h
@@ -18,49 +18,49 @@ CircularBuffer *GR_CircularBuffer_Create(uint16_t capacity);
 /// @brief Frees a CircularBuffer's all associated memories.
 ///        Note that any pointer stored by this buffer is not
 ///        freed by this function.
-/// @param buffer The pointer to the pointer to the CircularBuffer to free.
-void GR_CircularBuffer_Free(CircularBuffer **buffer);
+/// @param buffer_pp The pointer to the pointer to the CircularBuffer to free.
+void GR_CircularBuffer_Free(CircularBuffer **buffer_pp);
 
 /// @brief Get a CircularBuffer's maximum capacity.
-/// @param buffer The pointer to the CircularBuffer to inspect.
+/// @param buffer_ptr The pointer to the CircularBuffer to inspect.
 /// @return The buffer's capacity, in number of items. Returns zero if buffer is
 /// a null pointer.
-uint16_t GR_CircularBuffer_GetCapacity(CircularBuffer *buffer);
+uint16_t GR_CircularBuffer_GetCapacity(CircularBuffer *buffer_ptr);
 
 /// @brief Get a CircularBuffer's currently used size.
-/// @param buffer The pointer to the CircularBuffer to inspect.
+/// @param buffer_ptr The pointer to the CircularBuffer to inspect.
 /// @return The buffer's usage, in number of items stored. Returns 0 if buffer
 /// is a null pointer.
-uint16_t GR_CircularBuffer_GetCurrentSize(CircularBuffer *buffer);
+uint16_t GR_CircularBuffer_GetCurrentSize(CircularBuffer *buffer_ptr);
 
 /// @brief Check if a CircularBuffer is full.
-/// @param buffer The pointer to the CircularBuffer to inspect.
+/// @param buffer_ptr The pointer to the CircularBuffer to inspect.
 /// @return true if full, false otherwise. Returns true if buffer is a null
 /// pointer.
-bool GR_CircularBuffer_IsFull(CircularBuffer *buffer);
+bool GR_CircularBuffer_IsFull(CircularBuffer *buffer_ptr);
 
 /// @brief Check if a CircularBuffer is empty.
-/// @param buffer The pointer to the CircularBuffer to inspect.
+/// @param buffer_ptr The pointer to the CircularBuffer to inspect.
 /// @return true if empty, false otherwise. Returns true if buffer is a null
 /// pointer.
-bool GR_CircularBuffer_IsEmpty(CircularBuffer *buffer);
+bool GR_CircularBuffer_IsEmpty(CircularBuffer *buffer_ptr);
 
 /// @brief Push a copy of an item into a circular buffer.
-/// @param buffer Pointer to the CircularBuffer to store the item.
-/// @param object The item to be pushed into the CircularBuffer.
+/// @param buffer_ptr Pointer to the CircularBuffer to store the item.
+/// @param object_ptr A pointer to the item to be pushed into the CircularBuffer.
 /// @return 0 if the operation finished successfully, non-0 on error.
-uint8_t GR_CircularBuffer_Push(CircularBuffer *buffer, void *object);
+uint8_t GR_CircularBuffer_Push(CircularBuffer *buffer_ptr, void *object_ptr);
 
 /// @brief Removes a CircularBuffer's buffer head and retrives it.
-/// @param buffer The CircularBuffer in subject.
+/// @param buffer_ptr The CircularBuffer in subject.
 /// @return The pointer to the removed item. NULL if the buffer is null or is
 /// empty.
-void *GR_CircularBuffer_Pop(CircularBuffer *buffer);
+void *GR_CircularBuffer_Pop(CircularBuffer *buffer_ptr);
 
 /// @brief Retrives a CircularBuffer's buffer head.
-/// @param buffer The CircularBuffer in subject.
+/// @param buffer_ptr The CircularBuffer in subject.
 /// @return The pointer to the retrived item. NULL if the buffer is null or is
 /// empty.
-void *GR_CircularBuffer_Peek(CircularBuffer *buffer);
+void *GR_CircularBuffer_Peek(CircularBuffer *buffer_ptr);
 
 #endif
