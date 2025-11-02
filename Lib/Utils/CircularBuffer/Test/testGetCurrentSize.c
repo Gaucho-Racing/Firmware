e7eb9044ab735ccf8cf8fd64c29ca08c4509ee6b
Thomas Xu <thomas.xu.7@outlook.com>
2025-11-02 12:29:53 -0800

    Fix: Implement and test the memory-safe version of CircularBuffer_Lib (#95)
    # Circular Buffer
    
    ## Problem and Scope
    `GR_CircularBuffer_Peek` introduces a vulnerability: de-allocating items
    retrieved from this function and then calling `GR_CircularBuffer_Free`
    or `GR_CircularBuffer_Push` has a high chance of causing double-free
    error.
    
    Of course, this can be prevented by reinforcing usage rules specified in
    the **Gotchas and Limitations** section(which are also covered in
    README.md); however, I can also propose two solutions that will
    eliminate this vulnerability but will introduce extra memory overhead or
    reduce functionality respectively:
    1. Make `GR_CircularBuffer_Peek` return a fresh copy of buffer head,
    which will require our data structure to track each item's size; this
    will at best add 50% and at worst add 100% memory overhead to
    `CircularBuffer`.
    2. Remove the peek functionality, which obviously means that the user
    will no longer have a way to retrieve buffer head without modifying the
    buffer with better than $O(\text{itemSize}\times \text{currentSize})$
    efficiency.
    
    ## Description
    The CMake name of this library is CircularBuffer_Lib. This library
    defines a new type `CircularBuffer` and declares manipulating/inspecting
    functions. `CircularBuffer` is a fixed size queue that that stores
    references to structs allowing for message buffering.
    
    For more details please see README.md in the project folder.
    
    ## Gotchas and Limitations
    - If there are pointers within the objects pushed into the buffer, their
    addressed contents are not copied (It is not possible without
    reinforcing object types in the buffer).
    - The user is responsible for deallocating items retrieved by
    GR_CircularBuffer_Pop.
    - The user MUST NOT deallocate items retrieved by
    GR_CircularBuffer_Peek.
    - The user must deallocate the CircularBuffer's by using
    GR_CircularBuffer_Free.
    
    ## Testing
    
    - [x] HOOTL testing
    - [ ] HITL testing
    - [ ] Human tested
    
    ### Testing Details
    Every functions are tested for various cases. For details please see
    comments in the source codes of tests.
    
    ## Larger Impact
    This is a blocker for various other peripheral projects.
    
    ## Additional Context and Ticket
    Resolves #52 and #92
    
    Adds new features to #60
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- Lib/Utils/CircularBuffer/Test/testGetCurrentSize.c
+++ Lib/Utils/CircularBuffer/Test/testGetCurrentSize.c
@@ -16,7 +16,7 @@ int main()
 
 	// Test single item
 	int temp = 0;
-	GR_CircularBuffer_Push(buffer_ptr, &temp);
+	GR_CircularBuffer_Push(buffer_ptr, &temp, sizeof(temp));
 	if (GR_CircularBuffer_GetCurrentSize(buffer_ptr) != 1) {
 		GR_CircularBuffer_Free(&buffer_ptr);
 		return 2;
@@ -24,7 +24,7 @@ int main()
 	GR_CircularBuffer_Free(&buffer_ptr);
 
 	buffer_ptr = GR_CircularBuffer_Create(10);
-	GR_CircularBuffer_Push(buffer_ptr, &temp);
+	GR_CircularBuffer_Push(buffer_ptr, &temp, sizeof(temp));
 	if (GR_CircularBuffer_GetCurrentSize(buffer_ptr) != 1) {
 		GR_CircularBuffer_Free(&buffer_ptr);
 		return 3;
@@ -35,7 +35,7 @@ int main()
 	buffer_ptr = GR_CircularBuffer_Create(10);
 	int arr1[6] = {0};
 	for (int i = 0; i < 6; i++) {
-		GR_CircularBuffer_Push(buffer_ptr, &arr1[i]);
+		GR_CircularBuffer_Push(buffer_ptr, &arr1[i], sizeof(arr1[i]));
 	}
 	if (GR_CircularBuffer_GetCurrentSize(buffer_ptr) != 6) {
 		GR_CircularBuffer_Free(&buffer_ptr);
@@ -47,7 +47,7 @@ int main()
 	buffer_ptr = GR_CircularBuffer_Create(10);
 	int arr2[12] = {0};
 	for (int i = 0; i < 12; i++) {
-		GR_CircularBuffer_Push(buffer_ptr, &arr2[i]);
+		GR_CircularBuffer_Push(buffer_ptr, &arr2[i], sizeof(arr2[i]));
 	}
 	if (GR_CircularBuffer_GetCurrentSize(buffer_ptr) != 10) {
 		GR_CircularBuffer_Free(&buffer_ptr);
@@ -58,10 +58,10 @@ int main()
 	// Push and pop
 	buffer_ptr = GR_CircularBuffer_Create(10);
 	for (int i = 0; i < 9; i++) {
-		GR_CircularBuffer_Push(buffer_ptr, &arr2[i]);
+		GR_CircularBuffer_Push(buffer_ptr, &arr2[i], sizeof(arr2[i]));
 	}
 	for (int i = 0; i < 4; i++) {
-		GR_CircularBuffer_Pop(buffer_ptr);
+		free(GR_CircularBuffer_Pop(buffer_ptr));
 	}
 	if (GR_CircularBuffer_GetCurrentSize(buffer_ptr) != 5) {
 		GR_CircularBuffer_Free(&buffer_ptr);
@@ -73,10 +73,10 @@ int main()
 	buffer_ptr = GR_CircularBuffer_Create(10);
 	int arr4[15] = {0};
 	for (int i = 0; i < 15; i++) {
-		GR_CircularBuffer_Push(buffer_ptr, &arr4[i]);
+		GR_CircularBuffer_Push(buffer_ptr, &arr4[i], sizeof(arr4[i]));
 	}
 	for (int i = 0; i < 4; i++) {
-		GR_CircularBuffer_Pop(buffer_ptr);
+		free(GR_CircularBuffer_Pop(buffer_ptr));
 	}
 	if (GR_CircularBuffer_GetCurrentSize(buffer_ptr) != 6) {
 		GR_CircularBuffer_Free(&buffer_ptr);
