9f5640cbcb7bceb8cbdb7cc535618904b1d926c9
GabeMillikan <44247924+GabeMillikan@users.noreply.github.com>
2025-11-07 15:15:57 -0800

    Revert "use usart_example in G4HELLO"
    This reverts commit 1aba1517ac88ae4f399c5753c47d53eb08ee5f42.

--- BLINKY/G4HELLO/Core/Src/main.c
+++ BLINKY/G4HELLO/Core/Src/main.c
@@ -22,7 +22,6 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "Logomatic.h"
-#include "Peripherals/USART/usart.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -116,7 +115,6 @@ int main(void)
 		/* USER CODE BEGIN 3 */
 		LOGOMATIC("Hello, LOGOMATIC! Great to be here %f\n",
 			  3.14159265);
-		usart_example();
 
 		LL_mDelay(750);
 	}
