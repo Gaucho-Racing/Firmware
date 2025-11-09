2f596163b905fb01583793b19b7159fb5184178d
GabeMillikan <44247924+GabeMillikan@users.noreply.github.com>
2025-11-09 15:53:32 -0800

    stop using glob

--- Lib/Peripherals/USART/common.cmake
+++ Lib/Peripherals/USART/common.cmake
@@ -1,10 +1,8 @@
 add_library(PERIPHERAL_USART_LIB INTERFACE)
 
-set(_USART_DIR ${CMAKE_CURRENT_LIST_DIR})
-
-file(GLOB _USART_SOURCES "${_USART_DIR}/*.c")
-
-target_sources(PERIPHERAL_USART_LIB INTERFACE ${_USART_SOURCES})
+target_sources(PERIPHERAL_USART_LIB INTERFACE 
+    ${CMAKE_CURRENT_LIST_DIR}/usart.c
+)
 
 # Make headers accessible as #include "Peripherals/USART/usart.h"
-target_include_directories(PERIPHERAL_USART_LIB INTERFACE ${_USART_DIR}/../..)
+target_include_directories(PERIPHERAL_USART_LIB INTERFACE ${CMAKE_CURRENT_LIST_DIR}/../..)
