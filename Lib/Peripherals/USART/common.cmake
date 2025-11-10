4ff2735a2b7aaef92efc98ad569e1051fef2e62f
Gabe Millikan <44247924+GabeMillikan@users.noreply.github.com>
2025-11-09 21:05:50 -0800

    create `Lib/Peripherals/` to house upcoming peripheral abstractions (#107)

---
+++ Lib/Peripherals/USART/common.cmake
@@ -0,0 +1,10 @@
+add_library(PERIPHERAL_USART_LIB INTERFACE)
+
+target_sources(PERIPHERAL_USART_LIB INTERFACE ${CMAKE_CURRENT_LIST_DIR}/usart.c)
+
+# Make headers accessible as #include "Peripherals/USART/usart.h"
+target_include_directories(
+	PERIPHERAL_USART_LIB
+	INTERFACE
+		${CMAKE_CURRENT_LIST_DIR}/../..
+)
