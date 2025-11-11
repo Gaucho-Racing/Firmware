a9dc792114a359ab881cfd28c4b4427dc803f4cb
Colin-Bickel <cbickel@ucsb.edu>
2025-11-10 21:35:30 -0800

    Began work on the initialization function. Removed many enums to switch to LL-based input

--- Lib/FancyLayers-RENAME/SPI/Inc/spi.h
+++ Lib/FancyLayers-RENAME/SPI/Inc/spi.h
@@ -3,180 +3,30 @@
 #ifndef SPI_H
 #define SPI_H
 
-// ============================= Handler =============================
+typedef struct {
+    uint32_t CIPO_Pin, COPI_Pin, CS_Pin, SCLK_Pin;
+    uint32_t CIPO_Port, COPI_Port, CS_Port, SCLK_Port;
+    uint32_t Alternate_Function;
+    uint32_t SPIx_Number;
+} GR_SPI_Pins;
 
 // Generic type
 typedef struct {
     //LL structs
-    LL_SPI_InitTypeDef* config;
-    SPI_TypeDef* spi;
+    SPI_TypeDef* spi = nullptr;
+    uint32_t SPIx_Number;
     //GR structs
     CircularBuffer* buffer;
-} SPI_Handler;
+    //GPIO pin data
+    
+} GR_SPI_Handler;
 
 // Generic type
 typedef struct {
 
 } SPI_Message;
 
-// ============================= Enums =============================
-
-typedef enum {
-    MASTER,
-    SLAVE
-} GR_SPI_Mode;
-
-typedef enum {
-    DATAWIDTH4,
-    DATAWIDTH5,
-    DATAWIDTH6,
-    DATAWIDTH7,
-    DATAWIDTH8,
-    DATAWIDTH9,
-    DATAWIDTH10,
-    DATAWIDTH11,
-    DATAWIDTH12,
-    DATAWIDTH13,
-    DATAWIDTH14,
-    DATAWIDTH15,
-    DATAWIDTH16
-} GR_SPI_Data_Width;
-
-typedef enum {
-    FULL_DUPLEX,
-    SIMPLEX_RX,
-    HALF_DUPLEX_RX,
-    HALF_DUPLEX_TX
-} GR_SPI_Transfer_Direction;
-
-typedef enum {
-    HIGH,
-    LOW
-} GR_SPI_Clock_Polarity;
-
-typedef enum {
-    EDGE1,
-    EDGE2
-} GR_SPI_Clock_Phase;
-
-typedef enum {
-    SOFT,
-    HARD_INPUT,
-    HARD_OUTPUT
-} GR_SPI_NSS;
-
-typedef enum {
-    PSC_DIV2,
-    PSC_DIV4,
-    PSC_DIV8,
-    PSC_DIV16,
-    PSC_DIV32,
-    PSC_DIV64,
-    PSC_DIV128,
-    PSC_DIV256
-} GR_SPI_Baud_Rate_Prescalar;
-
-typedef enum {
-    LSB_FIRST,
-    MSB_FIRST
-} GR_SPI_Bit_Order;
-
-typedef enum {
-    CRCCalculationEnable,
-    CRCCalculationDisable
-} GR_SPI_CRC_Calculation;
-
-// ============================= Getter/Setter Functions =============================
-
-/**
- * @brief Sets the SPI transfer direction
- * 
- * @param handle
- * @return 
- */
-void GR_SPI_Set_Transfer_Direction(SPI_Handler* handle, GR_SPI_Transfer_Direction transfer_direction);
-
-/**
- * @brief Sets the SPI mode
- * 
- * @param handle
- * @return 
- */
-void GR_SPI_Set_Mode(SPI_Handler* handle, GR_SPI_Mode mode);
-
-/**
- * @brief Sets the SPI data width
- * 
- * @param handle
- * @return 
- */
-void GR_SPI_Set_Data_Width(SPI_Handler* handle, GR_SPI_Data_Width data_width);
-
-/**
- * @brief Sets the SPI clock polarity
- * 
- * @param handle
- * @return 
- */
-void GR_SPI_Set_Clock_Polarity(SPI_Handler* handle, GR_SPI_Clock_Polarity clock_polarity);
-
-/**
- * @brief Sets the SPI clock phase
- * 
- * @param handle
- * @return 
- */
-void GR_SPI_Set_Clock_Phase(SPI_Handler* handle, GR_SPI_Clock_Phase clock_phase);
-
-/**
- * @brief Sets the SPI NSS
- * 
- * @param handle
- * @return 
- */
-void GR_SPI_Set_NSS(SPI_Handler* handle, GR_SPI_NSS nss);
-
-/**
- * @brief Sets the SPI baud rate
- * 
- * @param handle
- * @return 
- */
-void GR_SPI_Set_Baud_Rate_Prescalar(SPI_Handler* handle, GR_SPI_Baud_Rate_Prescalar baud_rate_prescalar);
-
-/**
- * @brief Sets the SPI bit order
- * 
- * @param handle
- * @return 
- */
-void GR_SPI_Set_Bit_Order(SPI_Handler* handle, GR_SPI_Bit_Order bit_order);
-
-/**
- * @brief Sets the SPI CRC calculation
- * 
- * @param handle
- * @return 
- */
-void GR_SPI_Set_CRC_Calculation(SPI_Handler* handle, GR_SPI_CRC_Calculation crc_calculation);
-
-/**
- * @brief Sets the SPI crc poly. CRCPoly must be between 0x0000 and 0xFFFF.
- * 
- * @param handle
- * @return 
- */
-void GR_SPI_Set_CRC_Poly(SPI_Handler* handle, uint32_t crc_poly);
-
-// ============================= Initialization =============================
-
-/**
- * @brief Initializes SPI config to default values
- * 
- * @param handle
- * @return 
- */
-void GR_SPI_Setup_Config(SPI_Handler* handle) { LL_SPI_StructInit(handle->config); }
+// ============================= Handler Functions =============================
 
 /**
  * @brief Initializes SPI with config values
@@ -184,15 +34,27 @@ void GR_SPI_Setup_Config(SPI_Handler* handle) { LL_SPI_StructInit(handle->config
  * @param handle
  * @return 
  */
-void GR_SPI_Initialize(SPI_Handler* handle) { LL_SPI_Init(handle->spi, handle->config); }
+void GR_SPI_Initialize(SPI_Handler* handle, LL_SPI_InitTypeDef* config, GR_SPI_Pins *pins);
 
 /**
- * @brief Enable SPI
+ * @brief Tear down the SPI handler
  * 
- * @param config 
- * @return void 
+ * @param handler
  */
-void GR_SPI_Enable(SPI_Handler* handle);
+void GR_SPI_Close(SPI_Handler* handler);
+
+/**
+ * @brief Handles SPI interrupts
+ * 
+ * @param 
+ * @return 
+ */
+void GR_SPI_Interrupt_Handler(void);
+
+//Map SPI1-3 IRQHandlers to custom interrupt handler
+void SPI1_IRQHandler(void) { GR_SPI_Interrupt_Handler(); }
+void SPI2_IRQHandler(void) { GR_SPI_Interrupt_Handler(); }
+void SPI3_IRQHandler(void) { GR_SPI_Interrupt_Handler(); }
 
 // ============================= Tx/Rx =============================
 
@@ -212,11 +74,20 @@ void GR_SPI_Send(SPI_Handler* handler, SPI_Message data);
  */
 SPI_Message GR_SPI_Receive(SPI_Handler* handler);
 
+// ============================= Helper Functions =============================
+
 /**
- * @brief Shutdown the SPI handler
+ * @brief Returns the interrupt request number for a given SPIx peripheral
  * 
- * @param handler
+ * @param handle
  */
-void GR_SPI_Close(SPI_Handler* handler);
+uint32_t GR_SPI_Get_IRQn(uint32_t SPIx_number);
+
+/**
+ * @brief Enables the GPIO port clocks and SPI clock needed for the given pins
+ * 
+ * @param pins
+ */
+void GR_SPI_Enable_Clocks(GR_SPI_Pins* pins);
 
 #endif
--- Lib/FancyLayers-RENAME/SPI/Src/spi.c
+++ Lib/FancyLayers-RENAME/SPI/Src/spi.c
@@ -1,2 +1,37 @@
 // Wonderful SPI Abstraction Layer courtesy of Bailey
+#include "spi.h"
 
+void GR_SPI_Initialize(SPI_Handler* handle, LL_SPI_InitTypeDef* config, GR_SPI_Pins* pins) {
+    //Enable GPIO and SPI clocks
+    GR_SPI_Enable_Clocks(pins);
+    //Configure GPIOs for alternate function and pin mode, pull-up/pull-down, speed, and output type
+    LL_GPIO_SetPinMode(pins.CIPO_Port, pins.CIPO_Pin, LL_GPIO_MODE_ALTERNATE);
+    LL_GPIO_SetPinMode(pins.COPI_Port, pins.COPI_Pin, LL_GPIO_MODE_ALTERNATE);
+    LL_GPIO_SetPinMode(pins.SCLK_Port, pins.SCLK_Pin, LL_GPIO_MODE_ALTERNATE);
+    LL_GPIO_SetPinMode(pins.CS_Port, pins.CS_Pin, LL_GPIO_MODE_ALTERNATE);
+    
+    //Configure SPI protocol with config values
+    LL_SPI_Init(handle->spi, config);
+    //Enable SPI peripheral
+    LL_SPI_Enable(handle->spi);
+    //Enable interrupts in NVIC
+    uint32_t SPI_IRQn = GR_SPI_Get_IRQn(pins->SPIx_Number);
+    NVIC_SetPriority(SPI_IRQn, 1);
+    NVIC_EnableIRQ(SPI_IRQn);
+    //Enable interrupts at peripheral level
+    LL_SPI_EnableIT_ERR(handle->spi); // Error interrupt
+    LL_SPI_EnableIT_RXNE(handle->spi); // Not empty Rx buffer
+    // Consider enabling the empty Tx buffer interrupt (TXE)
+}
+
+void GR_SPI_Interrupt_Handler(void) {
+    //STUB
+}
+
+uint32_t GR_SPI_Get_IRQn(uint32_t SPIx_number) {
+    return 0; //STUB
+}
+
+void GR_SPI_Enable_Clocks(GR_SPI_Pins* pins) {
+    //STUB
+}
