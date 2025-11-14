913862ab450222fc1c7fb7d73152a28d9fe41553
Colin-Bickel <cbickel@ucsb.edu>
2025-11-13 21:06:01 -0800

    Got rid of pins struct and finished initialization rough draft. Started on SPI send()

--- Lib/FancyLayers-RENAME/SPI/Inc/spi.h
+++ Lib/FancyLayers-RENAME/SPI/Inc/spi.h
@@ -3,30 +3,30 @@
 #ifndef SPI_H
 #define SPI_H
 
-typedef struct {
-    uint32_t num_pins = 4;
-    uint32_t pin_nums[4];
-    uint32_t ports[4];
-    uint32_t alternate_function_number;
-    uint32_t SPIx_number;
-} GR_SPI_Pins;
+#define GR_SPI_UNKNOWN_IRQN -64
 
 // Generic type
 typedef struct {
     //LL structs
-    SPI_TypeDef* spi = nullptr;
-    uint32_t SPIx_Number;
-    GPIO_TypeDef* pins[4];
+    SPI_TypeDef* SPIx; // Pointer to SPI register wrapper (e.g. SPI1, SPI2, SPI3 macro defines)
+    //COPI, CIPO, SCLK, CS
+    GPIO_TypeDef* GPIOx[4]; // Pointer to GPIO port register wrapper (e.g. GPIOA, GPIOB, GPIOC, etc. macro defines)
+    //COPI, CIPO, SCLK, CS
+    uint32_t pins[4]; // SPI pin numbers (e.g. LL_GPIO_PIN_0, LL_GPIO_PIN_1, LL_GPIO_PIN_2 macro defines)
     uint32_t num_pins = 4;
     //GR structs
     CircularBuffer* buffer;
     //GPIO pin data
-    
+    SPI_message cur_message;
+    volatile uint16_t tx_index;
+    volatile uint16_t rx_index;
+    volatile uint8_t ongoing;
 } GR_SPI_Handler;
 
 // Generic type
 typedef struct {
-
+    uint8_t *tx_data;
+    uint16_t size;
 } SPI_Message;
 
 // ============================= Handler Functions =============================
@@ -37,14 +37,14 @@ typedef struct {
  * @param handle
  * @return 
  */
-void GR_SPI_Initialize(SPI_Handler* handle, LL_SPI_InitTypeDef* config, GR_SPI_Pins *pins);
+void GR_SPI_Initialize(GR_SPI_Handler* handle, LL_SPI_InitTypeDef* config, uint32_t alternate_function_num);
 
 /**
  * @brief Tear down the SPI handler
  * 
  * @param handler
  */
-void GR_SPI_Close(SPI_Handler* handler);
+void GR_SPI_Close(GR_SPI_Handler* handler);
 
 /**
  * @brief Handles SPI interrupts
@@ -67,7 +67,7 @@ void SPI3_IRQHandler(void) { GR_SPI_Interrupt_Handler(); }
  * @param handler
  * @param data
  */
-void GR_SPI_Send(SPI_Handler* handler, SPI_Message data);
+void GR_SPI_Send(GR_SPI_Handler* handler, SPI_Message data);
 
 /**
  * @brief Read from the SPI buffer; primarily intended for polling. Rely on the passed in handler when setup() is called 
@@ -75,7 +75,8 @@ void GR_SPI_Send(SPI_Handler* handler, SPI_Message data);
  * @param handler
  * @return SPI_Message 
  */
-SPI_Message GR_SPI_Receive(SPI_Handler* handler);
+
+SPI_Message GR_SPI_Receive(GR_SPI_Handler* handler);
 
 // ============================= Helper Functions =============================
 
@@ -84,14 +85,14 @@ SPI_Message GR_SPI_Receive(SPI_Handler* handler);
  * 
  * @param handle
  */
-uint32_t GR_SPI_Get_IRQn(uint32_t SPIx_number);
+uint32_t GR_SPI_Get_IRQn(SPI_TypeDef* SPIx);
 
 /**
  * @brief Enables the GPIO port clocks and SPI clock needed for the given pins
  * 
  * @param pins
  */
-void GR_SPI_Enable_Clocks(GR_SPI_Pins* pins);
+void GR_SPI_Enable_Clocks(GR_SPI_Handler* handle);
 
 /**
  * @brief Configures GPIO pins for SPI
@@ -99,6 +100,6 @@ void GR_SPI_Enable_Clocks(GR_SPI_Pins* pins);
  * @param config
  * @param pins
  */
-void GR_SPI_Configure_Pins(LL_GPIO_InitTypeDef* pin_config, GR_SPI_Pins* pins)
+void GR_SPI_Configure_Pins(GR_SPI_Handler* handle, LL_GPIO_InitTypeDef* pin_config, uint32_t alternate_function_num);
 
 #endif
--- Lib/FancyLayers-RENAME/SPI/Src/spi.c
+++ Lib/FancyLayers-RENAME/SPI/Src/spi.c
@@ -1,51 +1,145 @@
 // Wonderful SPI Abstraction Layer courtesy of Bailey
 #include "spi.h"
+#define spi_status 0
+#define spi_ongoing -1
 
-void GR_SPI_Initialize(SPI_Handler* handle, LL_SPI_InitTypeDef* config, GR_SPI_Pins* pins) {
+void GR_SPI_Initialize(GR_SPI_Handler* handle, LL_SPI_InitTypeDef* config, uint32_t alternate_function_num) {    
+    //Copy necessary pin references
+    handle->SPIx = pins->SPIx;
+    handle->num_pins = pins->num_pins;
+    for(int i = 0; i < pins->num_pins; i++) {
+        handle->GPIOx[i] = pins->GPIOx[i];
+    }
+    
     //Enable GPIO and SPI clocks
     GR_SPI_Enable_Clocks(pins);
 
     //Configure GPIOs
     LL_GPIO_InitTypeDef pin_config;
-    GR_SPI_Configure_Pins(&pin_config, pins);
+    GR_SPI_Configure_Pins(handle, &pin_config, pins);
 
     //Configure SPI protocol with config values
     LL_SPI_Init(handle->spi, config);
+
     //Enable SPI peripheral
     LL_SPI_Enable(handle->spi);
+
     //Enable interrupts in NVIC
-    uint32_t SPI_IRQn = GR_SPI_Get_IRQn(pins->SPIx_Number);
-    NVIC_SetPriority(SPI_IRQn, 1);
-    NVIC_EnableIRQ(SPI_IRQn);
+    uint32_t SPI_IRQn = GR_SPI_Get_IRQn(pins->SPIx);
+    if(SPI_IRQn != GR_SPI_UNKNOWN_IRQN) {
+        NVIC_SetPriority(SPI_IRQn, 1);
+        NVIC_EnableIRQ(SPI_IRQn);
+    } else {
+        //Throw an error
+    }
+    
     //Enable interrupts at peripheral level
     LL_SPI_EnableIT_ERR(handle->spi); // Error interrupt
     LL_SPI_EnableIT_RXNE(handle->spi); // Not empty Rx buffer
-    // Consider enabling the empty Tx buffer interrupt (TXE)
+    LL_SPI_EnableIT_TXE(handle->spi); // Empty Tx buffer
 }
 
 void GR_SPI_Interrupt_Handler(void) {
     //STUB
 }
 
-uint32_t GR_SPI_Get_IRQn(uint32_t SPIx_number) {
-    return 0; //STUB
-}
-
-void GR_SPI_Enable_Clocks(GR_SPI_Pins* pins) {
-    for(int i = 0; i < pins->num_pins; i++) {
-
+uint32_t GR_SPI_Get_IRQn(SPI_TypeDef* SPIx) {
+    switch(SPIx) {
+        case SPI1: return SPI1_IRQn; //35
+        case SPI2: return SPI2_IRQn; //36
+        case SPI3: return SPI3+IRQn; //51
+        default: return GR_SPI_UNKNOWN_IRQN;
     }
 }
 
-void GR_SPI_Configure_Pins(LL_GPIO_InitTypeDef* pin_config, GR_SPI_Pins* pins) {
+void GR_SPI_Enable_Clocks(GR_SPI_Handler* handle) {
+    //Enable GPIO clock
+    uint32_t GPIOx_Port
+    for(int i = 0; i < handle->num_pins; i++) {
+        switch(handle->GPIOx[i]) {
+            case GPIOA:
+                GPIOx_Port = LL_AHB2_GRP1_PERIPH_GPIOA;
+                break;
+            case GPIOB:
+                GPIOx_Port = LL_AHB2_GRP1_PERIPH_GPIOB;
+                break;
+            case GPIOC:
+                GPIOx_Port = LL_AHB2_GRP1_PERIPH_GPIOC;
+                break;
+            case GPIOD:
+                GPIOx_Port = LL_AHB2_GRP1_PERIPH_GPIOD;
+                break;
+            case GPIOE:
+                GPIOx_Port = LL_AHB2_GRP1_PERIPH_GPIOE;
+                break;
+            case GPIOF:
+                GPIOx_Port = LL_AHB2_GRP1_PERIPH_GPIOF;
+                break;
+            case GPIOG:
+                GPIOx_Port = LL_AHB2_GRP1_PERIPH_GPIOG;
+                break;
+            case GPIOH:
+                GPIOx_Port = LL_AHB2_GRP1_PERIPH_GPIOH;
+                break;
+            default: //Do nothing (unknown GPIOx)
+        }
+        LL_AHB2_GRP1_EnableClock(GPIOx_Port);
+    }
+    
+    //Enable SPI clock
+    switch(handle->SPIx) {
+        case SPI1:
+            LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SPI1);
+            break;
+        case SPI2:
+            LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_SPI2);
+            break;
+        case SPI3:
+            LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_SPI3);
+            break;
+        default: //Do nothing (unknown SPIx)
+    }
+}
+
+void GR_SPI_Send(GR_SPI_Handler* handler, SPI_Message data) {
+    if(handler->ongoing){
+        return spi_ongoing; 
+    }
+    handler->ongoing = 1;
+    handler->tx_index = 0;
+    handler->rx_index = 0;
+    handler->cur_msg = *msg;
+
+    if (handler->CS_Port != NULL) {
+        LL_GPIO_ResetOutputPin(handler->GPI0x[3], handler->pins[3]);
+    }
+
+    while (LL_SPI_IsActiveFlag_RXNE(handler->SPI1)) {
+        if (LL_SPI_GetDataWidth(handler->SPI1) == LL_SPI_DATAWIDTH_16BIT) {
+            (void)LL_SPI_ReceiveData16(handler->SPI1);
+        } else {
+            (void)LL_SPI_ReceiveData8(handler->SPI1);
+        }
+    }
+
+    LL_SPI_EnableIT_TXE(handler->SPI1);
+    LL_SPI_EnableIT_RXNE(handler->SPI1);
+    return spi_status;
+}
+
+void GR_SPI_Configure_Pins(GR_SPI_Handler* handle, LL_GPIO_InitTypeDef* pin_config, uint32_t alternate_function_num) {
     LL_GPIO_StructInit(pin_config); // Default config values
     pin_config.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH; // Very high output speed
     pin_config.Pull = LL_GPIO_PULL_NO; //No pull-up or pull-down resistance
     pin_config.OutputType = LL_GPIO_OUTPUT_PUSHPULL; // Push-pull output (not open-drain)
     pin_config.Mode = LL_GPIO_MODE_ALTERNATE; // Alternate pin function mode
-    pin_config.Alternate = pins->alternate_function_number; // Alternate function number
-    for(int i = 0; i < pins->num_pins; i++) {
-        pin_config.Pin = pins->pin_nums[i];
+    pin_config.Alternate = alternate_function_num; // Alternate function number
+    for(int i = 0; i < handle->num_pins; i++) {
+        pin_config.Pin = handle->pins[i];
         LL_GPIO_Init(handle->pins[i], &pin_config);
     }
+}
+
+SPI_Message GR_SPI_Receive(GR_SPI_Handler* handler){
+    
 }
