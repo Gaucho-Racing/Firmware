3b7d1f60fd5ffbc2c93fe3ab76411bc439572bf6
kai h <splattack@169-231-175-233.wireless.ucsb.edu>
2025-11-18 16:40:54 -0800

    tidied up DMA init function to be blank

--- Underglow/Core/Src/main.c
+++ Underglow/Core/Src/main.c
@@ -81,41 +81,7 @@ static void DMA_LL_Init();
 
 static void DMA_LL_Init(void) {
 
-  LL_SPI_InitTypeDef SPI_InitStruct = {0};
-  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  // Enable peripheral and GPIO clocks
-  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_SPI2);
-  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOB);
-
-  // Configure GPIO pins for SPI2: PB13=SCK, PB14=MISO, PB15=MOSI
-  GPIO_InitStruct.Pin = LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15;
-  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-  GPIO_InitStruct.Alternate = LL_GPIO_AF_5;
-  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
-  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-  LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-
-  SPI_InitStruct.Mode = LL_SPI_MODE_MASTER;
-  SPI_InitStruct.TransferDirection = LL_SPI_HALF_DUPLEX_RX;
-  SPI_InitStruct.DataWidth = LL_SPI_DATAWIDTH_8BIT;
-  SPI_InitStruct.ClockPolarity = LL_SPI_POLARITY_LOW;
-  SPI_InitStruct.ClockPhase = LL_SPI_PHASE_1EDGE;
-  SPI_InitStruct.NSS = LL_SPI_NSS_SOFT;
-  SPI_InitStruct.BaudRate = LL_SPI_BAUDRATEPRESCALER_DIV32;
-  SPI_InitStruct.BitOrder = LL_SPI_MSB_FIRST;
-  SPI_InitStruct.CRCCalculation = LL_SPI_CRCCALCULATION_DISABLE;
-
-  // TODO: DMA set up?
-
-  if (LL_SPI_Init(SPI2, &SPI_InitStruct) != SUCCESS)
-  {
-    Error_Handler();
-  }
-
-  LL_SPI_Enable(SPI2);
 
 }
 
