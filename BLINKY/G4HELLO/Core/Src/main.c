ffa9c46561b281fb1ba1484f3ecab34629949f07
Daniel Hansen <dchansen06@gmail.com>
2025-12-16 22:31:02 -0800

    Remove unused LPUART1 initialization function from G4HELLO `main.c`

--- BLINKY/G4HELLO/Core/Src/main.c
+++ BLINKY/G4HELLO/Core/Src/main.c
@@ -49,7 +49,6 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_LPUART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -206,78 +205,6 @@ void SystemClock_Config(void)
 	if (HAL_InitTick(TICK_INT_PRIORITY) != HAL_OK) {
 		Error_Handler();
 	}
-}
-
-/**
- * @brief LPUART1 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_LPUART1_UART_Init(void)
-{
-
-	/* USER CODE BEGIN LPUART1_Init 0 */
-
-	/* USER CODE END LPUART1_Init 0 */
-
-	LL_LPUART_InitTypeDef LPUART_InitStruct = {0};
-
-	LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-	LL_RCC_SetLPUARTClockSource(LL_RCC_LPUART1_CLKSOURCE_PCLK1);
-
-	/* Peripheral clock enable */
-	LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_LPUART1);
-
-	LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
-	/**LPUART1 GPIO Configuration
-	PA2   ------> LPUART1_TX (ST-LINK VCP)
-	PA3   ------> LPUART1_RX (ST-LINK VCP)
-	*/
-	GPIO_InitStruct.Pin = LL_GPIO_PIN_2;
-	GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-	GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-	GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-	GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-	GPIO_InitStruct.Alternate = LL_GPIO_AF_12;
-	LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-	GPIO_InitStruct.Pin = LL_GPIO_PIN_3;
-	GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-	GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-	GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-	GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-	GPIO_InitStruct.Alternate = LL_GPIO_AF_12;
-	LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-	/* USER CODE BEGIN LPUART1_Init 1 */
-
-	/* USER CODE END LPUART1_Init 1 */
-	LPUART_InitStruct.PrescalerValue = LL_LPUART_PRESCALER_DIV1;
-	LPUART_InitStruct.BaudRate = 115200;
-	LPUART_InitStruct.DataWidth = LL_LPUART_DATAWIDTH_8B;
-	LPUART_InitStruct.StopBits = LL_LPUART_STOPBITS_1;
-	LPUART_InitStruct.Parity = LL_LPUART_PARITY_NONE;
-	LPUART_InitStruct.TransferDirection = LL_LPUART_DIRECTION_TX_RX;
-	LL_LPUART_Init(LPUART1, &LPUART_InitStruct);
-	LL_LPUART_SetTXFIFOThreshold(LPUART1, LL_LPUART_FIFOTHRESHOLD_1_8);
-	LL_LPUART_SetRXFIFOThreshold(LPUART1, LL_LPUART_FIFOTHRESHOLD_1_8);
-	LL_LPUART_DisableFIFO(LPUART1);
-	LL_LPUART_SetWakeUpMethod(LPUART1, LL_LPUART_WAKEUP_IDLELINE);
-
-	/* USER CODE BEGIN WKUPType LPUART1 */
-
-	/* USER CODE END WKUPType LPUART1 */
-
-	LL_LPUART_Enable(LPUART1);
-
-	/* Polling LPUART1 initialisation */
-	while ((!(LL_LPUART_IsActiveFlag_TEACK(LPUART1))) ||
-	       (!(LL_LPUART_IsActiveFlag_REACK(LPUART1)))) {
-	}
-	/* USER CODE BEGIN LPUART1_Init 2 */
-
-	/* USER CODE END LPUART1_Init 2 */
 }
 
 /**
