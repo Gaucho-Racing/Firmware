c56e37c2b15cc6cbf7d3cb7734f34501eb619335
Daniel Hansen <dchansen06@gmail.com>
2025-12-31 15:03:08 -0800

    Add skeleton work to be able to merge in new `main` changes

---
+++ ECU/Application/Inc/CANdler.h
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "GR_OLD_MSG_ID.h"
+#include "GR_OLD_NODE_ID.h"
+
+#ifndef CANDLER_H
+#define CANDLER_H
+
+bool ECU_CAN_MessageHandler(GR_OLD_BUS_ID bus_id, GR_OLD_MSG_ID msg_id, GR_OLD_NODE_ID sender_id, uint8_t* data, uint8_t data_length, uint32_t timestamp);
+
+#endif
