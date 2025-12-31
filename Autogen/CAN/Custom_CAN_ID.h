dc21a6ae8ef880477d972dee4c25dd6c740e8f1e
Nandini <nandinim.dharwadkar@gmail.com>
2025-12-31 11:24:07 -0800

    custom id header

--- Autogen/CAN/Custom_CAN_ID.h
+++ Autogen/CAN/Custom_CAN_ID.h
@@ -1,9 +1,11 @@
 // Auto-generated Custom CAN ID header
-#ifndef CUSTOM_CAN_IDS_H
-#define CUSTOM_CAN_IDS_H
+#ifndef CUSTOM_CAN_ID_H
+#define CUSTOM_CAN_ID_H
 
 typedef enum {
+    DTI_CONTROL_1_CAN_ID = 116,
+    DTI_CONTROL_7_CAN_ID = 716,
+    DTI_DATA_5_CAN_ID = 2416,
 } CUSTOM_CAN_ID_t;
 
-
-#endif // Custom_CAN_IDS_H
+#endif // CUSTOM_CAN_ID_H
