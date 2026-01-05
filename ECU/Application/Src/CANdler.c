9aba2cfe0a2b037f29cb1dca44ae77cc730e67c0
Daniel Hansen <dchansen06@gmail.com>
2026-01-05 01:15:21 -0800

    Add TODO comments for state data handling in message reporting functions

--- ECU/Application/Src/CANdler.c
+++ ECU/Application/Src/CANdler.c
@@ -11,6 +11,7 @@
 void ReportBadMessageLength(GR_OLD_BUS_ID bus_id, GR_OLD_MSG_ID msg_id,
 			    GR_OLD_NODE_ID sender_id)
 {
+	// TODO Ideally change some state data to note a bad message, ie if BCU that can be a comms error
 	LOGOMATIC("Bad ECU CAN Rx length! Bus: %d, Msg: %X, Sender: %X\n",
 		  bus_id, msg_id, sender_id);
 }
@@ -18,6 +19,7 @@ void ReportBadMessageLength(GR_OLD_BUS_ID bus_id, GR_OLD_MSG_ID msg_id,
 void ReportUnhandledMessage(GR_OLD_BUS_ID bus_id, GR_OLD_MSG_ID msg_id,
 			    GR_OLD_NODE_ID sender_id)
 {
+	// Filtering likely needs to be adjusted if this is happening often
 	LOGOMATIC("Unhandled ECU CAN Rx msg! Bus: %d, Msg: %X, Sender: %X\n",
 		  bus_id, msg_id, sender_id);
 }
