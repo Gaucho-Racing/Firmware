84df2f356c7632797c63e06ae90a4dd15d8a07d2
Daniel Hansen <dchansen06@gmail.com>
2026-01-09 03:09:28 -0800

    Add target_link_libraries for ADC to link with GLOBALSHARE_LIB

--- Lib/FancyLayers-RENAME/ADC/adc.cmake
+++ Lib/FancyLayers-RENAME/ADC/adc.cmake
@@ -3,3 +3,5 @@ add_library(ADC INTERFACE)
 target_sources(ADC INTERFACE ${CMAKE_CURRENT_LIST_DIR}/Src/gr_adc.c)
 
 target_include_directories(ADC INTERFACE ${CMAKE_CURRENT_LIST_DIR}/Inc)
+
+target_link_libraries(ADC INTERFACE GLOBALSHARE_LIB)
