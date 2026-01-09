d3c93228d2d17c24c7460111608234ff0215315c
Casey Zwicker <caseypzwicker@gmail.com>
2026-01-08 21:58:59 -0800

    The beginnings of state machine implementation
    Co-authored-by: Ng0710 <Ng0710@users.noreply.github.com>
    Co-authored-by: Thomas Xu <XuChe777@users.noreply.github.com>

--- ECU/Application/Inc/StateData.h
+++ ECU/Application/Inc/StateData.h
@@ -19,11 +19,21 @@
  * logic to access and modify the ECU's operational data.
  */
 typedef struct ECU_StateData {
-	// DON'T TOUCH YET
-	GR_OLD_ECU_STATUS_1_MSG ecuStatus1;
-	GR_OLD_ECU_STATUS_2_MSG ecuStatus2;
-	GR_OLD_ECU_STATUS_3_MSG ecuStatus3;
 
+	// These states are copied from the GR_OLD_ECU_STATUS_#_MSG structs
+	// TODO: Remove unneeded states
+	int8_t ping_block[3]; /** Node timeout status bits (1=OK, 0=Timeout) */
+	uint8_t powerlevel_torquemap; /** Power lvl (4b) & torque map (4b) */
+	uint8_t max_cell_temp;	      /** Hottest cell temp, 0.25°C/bit */
+	uint8_t tractivebattery_soc;  /** Accumulator SoC, 20x/51=% */
+	uint8_t glv_soc;	      /** GLV SoC, 20x/51=% */
+	uint16_t vehicle_speed; /** Vehicle speed, 0.01 MPH */
+	uint16_t fr_wheel_rpm;	/** FR wheel, 0.1x-3276.8 RPM */
+	uint16_t fl_wheel_rpm;	/** FL wheel, 0.1x-3276.8 RPM */
+	uint16_t rr_wheel_rpm; /** RR wheel, 0.1x-3276.8 RPM */
+	uint16_t rl_wheel_rpm; /** RL wheel, 0.1x-3276.8 RPM */
+
+	GR_ECU_State ecu_state;
 	int32_t dischargeStartMillis;
 	uint32_t lastECUStatusMsgTick;
 	uint32_t lastTSSIFlash;
@@ -39,6 +49,7 @@ typedef struct ECU_StateData {
 	uint8_t inverter_fault_map;
 	bool bse_apps_violation;
 	bool ts_active_button_engaged;
+	uint16_t ts_voltage;	/** TS output voltage, 0.01V */
 	bool rtd_button_engaged;
 } ECU_StateData; // FIXME Add comments to each data field with descriptions and
 		 // rules (eg -1 = invalid?, etc)
