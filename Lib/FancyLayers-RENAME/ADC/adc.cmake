6f76fbb753da1db565f5a891ebf5cf892a73b03b
Anthony Ma <anthony497@ucsb.edu>
2026-01-29 20:25:53 -0800

    New and Improved ADC + EMA (#186)
    # ADC
    
    ## Problem and Scope
    A classic sliding window function needs to track a certain number of
    previous ADC frame values for various different signals. This uses
    unnecessary memory, so we can fix this by using an exponential moving
    average. This directly smooths the ADC output based on an exponential
    output and the current frame.
    
    ## Description
    We implemented
    `ADC_UpdateAnalogValues_EMA(volatile uint16_t *new_values, int
    num_signals, double alpha, uint16_t *weighted_output)`
    following
    https://en.wikipedia.org/wiki/Exponential_smoothing#Basic_(simple)_exponential_smoothing.
    It returns the exponential moving average instead of a sliding window
    function to reduce memory usage (due to the window of previous ADC
    values).
    
    ## Gotchas and Limitations
    If the data are correlated in some way (i.e. a linear function), a
    simple exponential smoothing function will always lag behind and never
    reach the true value of the signal.
    A solution would be to use Holt double exponential smoothing, which
    attempts to estimate the trend. This operation is still much faster than
    the sliding window function since we are again only looking at previous
    and new values and does not require more memory.
    
    ## Testing
    
    - [ ] HOOTL testing
    - [ ] HITL testing
    - [x] Human tested
    
    ### Testing Details
    We tested this on a G4 board and played around with the weights.
    
    ## Larger Impact
    A working ADC with a customizable smoothing function and weight is
    essential for all other signals to be smoothed.
    
    ## Additional Context and Ticket
    #170
    
    ---------
    
    Signed-off-by: Daniel Hansen <105574022+dchansen06@users.noreply.github.com>
    Signed-off-by: rsargis <ryan_sargis@ucsb.edu>
    Signed-off-by: Anthony Ma <anthony497@ucsb.edu>
    Co-authored-by: ManasijaBhargavan <manas@ucsb.edu>
    Co-authored-by: planeflight <olivier.lvannier@gmail.com>
    Co-authored-by: Daniel Hansen <105574022+dchansen06@users.noreply.github.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: DavidUzunov <david.uzunov@gmail.com>
    Co-authored-by: David Uzunov <DavidUzunov@users.noreply.github.com>
    Co-authored-by: Shravya Salem Sathish <Shrav816@users.noreply.github.com>
    Co-authored-by: Shravya Salem Sathish <shravyassathish@gmail.com>
    Co-authored-by: Daniel Hansen <dchansen06@gmail.com>
    Co-authored-by: William Ou <WilliamOu@users.noreply.github.com>
    Co-authored-by: Manas Bhargavan <ManasijaBhargavan@users.noreply.github.com>
    Co-authored-by: rsargis <ryan_sargis@ucsb.edu>

--- Lib/FancyLayers-RENAME/ADC/adc.cmake
+++ Lib/FancyLayers-RENAME/ADC/adc.cmake
@@ -1,7 +1,7 @@
-add_library(ADC INTERFACE)
+add_library(GR_ADC INTERFACE)
 
-target_sources(ADC INTERFACE ${CMAKE_CURRENT_LIST_DIR}/Src/gr_adc.c)
+target_sources(GR_ADC INTERFACE ${CMAKE_CURRENT_LIST_DIR}/Src/gr_adc.c)
 
-target_include_directories(ADC INTERFACE ${CMAKE_CURRENT_LIST_DIR}/Inc)
+target_include_directories(GR_ADC INTERFACE ${CMAKE_CURRENT_LIST_DIR}/Inc)
 
-target_link_libraries(ADC INTERFACE GLOBALSHARE_LIB)
+target_link_libraries(GR_ADC INTERFACE GLOBALSHARE_LIB)
