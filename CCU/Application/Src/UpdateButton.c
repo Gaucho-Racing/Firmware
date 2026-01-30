1e4f94e3be12e3a9c88674f01c836ccb32bce3ca
Amy Saffer <amy_saffer@ucsb.edu>
2026-01-29 20:27:07 -0800

    Check_Button state data fixed

--- CCU/Application/Src/UpdateButton.c
+++ CCU/Application/Src/UpdateButton.c
@@ -6,12 +6,12 @@
 void Check_Button(CCU_StateData *state_data)
 {
 	if (LL_GPIO_IsInputPinSet(GPIOC, LL_GPIO_PIN_13)) {
-		if (state_data.Button_Status == 0) {
-			state_data.Button_Status = 1;
+		if (state_data->Button_Status == 0) {
+			state_data->Button_Status = 1;
 			LOGOMATIC("On\n");
 
 		} else {
-			state_data.Button_Status = 0;
+			state_data->Button_Status = 0;
 			LOGOMATIC("Off\n");
 		}
 	}
