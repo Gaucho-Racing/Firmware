6b174f4abd74dfd39fcea750fbba5e9514f558c9
Bailey Say <baileyasay@gmail.com>
2026-02-05 21:02:56 -0800

    initialize dash panel can stuff
    Co-authored-by: David Uzunov <DavidUzunov@users.noreply.github.com>
    Co-authored-by: Anthony Ma <anthony497@ucsb.edu>
    Co-authored-by: Aagrim Hoysal <ahoysal@users.noreply.github.com>

---
+++ DashPanel/Application/Src/CANdler.c
@@ -0,0 +1,75 @@
+#include "can.h"
+#include "stm32g4xx_hal_fdcan.h"
+#include "CANdler.h"
+
+#define ECU_ID 1 // ID of correct ECU message - TODO: change with correct ID
+#define PING_ID 2 // ID of ping message - TODO: change with correct ID
+
+void CANInitialize() {
+    CANConfig canCfg;
+
+	canCfg.hal_fdcan_init.ClockDivider = FDCAN_CLOCK_DIV1;
+	canCfg.hal_fdcan_init.FrameFormat = FDCAN_FRAME_FD_NO_BRS;
+	canCfg.hal_fdcan_init.TxFifoQueueMode = FDCAN_TX_FIFO_OPERATION;
+	canCfg.hal_fdcan_init.Mode = FDCAN_MODE_NORMAL;
+	canCfg.hal_fdcan_init.AutoRetransmission = ENABLE;
+	canCfg.hal_fdcan_init.TransmitPause = DISABLE;
+	canCfg.hal_fdcan_init.ProtocolException = ENABLE;
+	canCfg.hal_fdcan_init.NominalPrescaler = 1;
+	canCfg.hal_fdcan_init.NominalSyncJumpWidth = 16;
+	canCfg.hal_fdcan_init.NominalTimeSeg1 = 127; // Updated for 170MHz: (1+127+42)*1 = 170 ticks -> 1 Mbps
+	canCfg.hal_fdcan_init.NominalTimeSeg2 = 42;
+	canCfg.hal_fdcan_init.DataPrescaler = 8;
+	canCfg.hal_fdcan_init.DataSyncJumpWidth = 16;
+	canCfg.hal_fdcan_init.DataTimeSeg1 = 15; // Updated for 170MHz: (1+15+5)*8 = 168 ticks -> ~5 Mbps
+	canCfg.hal_fdcan_init.DataTimeSeg2 = 5;
+	canCfg.hal_fdcan_init.StdFiltersNbr = 1;
+	canCfg.hal_fdcan_init.ExtFiltersNbr = 0;
+
+	canCfg.rx_callback = CAN_callback;	  // PLEASE SET
+	canCfg.rx_interrupt_priority = 0; // PLEASE SET
+	canCfg.tx_interrupt_priority = 0; // PLEASE SET
+	canCfg.tx_buffer_length = 3;	  // PLEASE SET
+
+	// canCfg.rx_gpio = GPIOB;
+	// canCfg.init_rx_gpio.Pin = GPIO_PIN_12;
+	canCfg.init_rx_gpio.Mode = GPIO_MODE_AF_PP;
+	canCfg.init_rx_gpio.Pull = GPIO_PULLUP;
+	canCfg.init_rx_gpio.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+	// canCfg.init_rx_gpio.Alternate = GPIO_AF9_FDCAN2;
+
+	// canCfg.tx_gpio = GPIOB;
+	// canCfg.init_tx_gpio.Pin = GPIO_PIN_13;
+	canCfg.init_tx_gpio.Mode = GPIO_MODE_AF_PP;
+	canCfg.init_tx_gpio.Pull = GPIO_NOPULL;
+	canCfg.init_tx_gpio.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+	// canCfg.init_tx_gpio.Alternate = GPIO_AF9_FDCAN2;
+
+	// FDCAN_FilterTypeDef filter;
+	// can_add_filter(can2Handle, &filter);
+	/* USER CODE END 2 */
+
+
+	// FDCAN_TxHeaderTypeDef TxHeader = {
+	//     .Identifier = 1,
+
+	//     .IdType = FDCAN_STANDARD_ID,
+	//     .TxFrameType = FDCAN_DATA_FRAME,
+	//     .ErrorStateIndicator = FDCAN_ESI_ACTIVE, // honestly this might be a value you have to read from a node
+	// 					     // FDCAN_ESI_ACTIVE is just a state that assumes there are minimal errors
+	//     .DataLength = 1,
+	//     .BitRateSwitch = FDCAN_BRS_OFF,
+	//     .TxEventFifoControl = FDCAN_NO_TX_EVENTS, // change to FDCAN_STORE_TX_EVENTS if you need to store info regarding transmitted messages
+	//     .MessageMarker = 0			      // also change this to a real address if you change fifo control
+	// };
+	CANHandle *can_handler = can_init(&canCfg);
+	can_start(can_handler);
+}
+
+void CAN_callback(uint32_t ID, void *data, uint32_t size){
+    if (ID == ECU_ID) {
+        // process data
+    } else if (ID == PING_ID) {
+        // process ping
+    }
+}
--- DashPanel/Core/Src/main.c
+++ DashPanel/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "CANdler.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -97,6 +98,7 @@ int main(void)
 	LL_PWR_DisableUCPDDeadBattery();
 
 	/* USER CODE BEGIN Init */
+	CANInitialize();
 
 	/* USER CODE END Init */
 
@@ -129,28 +131,42 @@ int main(void)
  * @brief System Clock Configuration
  * @retval None
  */
+
 void SystemClock_Config(void)
 {
-	LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
-	while (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_0) {}
-	LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
+	LL_FLASH_SetLatency(LL_FLASH_LATENCY_4);
+	while (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_4) {}
+	LL_PWR_EnableRange1BoostMode();
 	LL_RCC_HSI_Enable();
 	/* Wait till HSI is ready */
 	while (LL_RCC_HSI_IsReady() != 1) {}
 
 	LL_RCC_HSI_SetCalibTrimming(64);
-	LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
+	LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_4, 85, LL_RCC_PLLR_DIV_2);
+	LL_RCC_PLL_EnableDomain_SYS();
+	LL_RCC_PLL_Enable();
+	/* Wait till PLL is ready */
+	while (LL_RCC_PLL_IsReady() != 1) {}
+
+	LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+	LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_2);
 	/* Wait till System clock is ready */
-	while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI) {}
+	while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL) {}
+
+	/* Insure 1us transition state at intermediate medium speed clock*/
+	for (__IO uint32_t i = (170 >> 1); i != 0; i--)
+		;
 
 	/* Set AHB prescaler*/
 	LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
 	LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
 	LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+	LL_SetSystemCoreClock(170000000);
 
-	LL_Init1msTick(16000000);
-
-	LL_SetSystemCoreClock(16000000);
+	/* Update the time base */
+	if (HAL_InitTick(TICK_INT_PRIORITY) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /**
