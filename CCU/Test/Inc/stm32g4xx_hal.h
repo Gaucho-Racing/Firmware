79028c0d63b65ee6e1e85764bc05d4775d25ce5a
David Wilson <wilsondavid0611@gmail.com>
2026-02-27 00:04:02 -0800

    small change

--- CCU/Test/Inc/stm32g4xx_hal.h
+++ CCU/Test/Inc/stm32g4xx_hal.h
@@ -15,7 +15,7 @@ uint32_t HAL_GetTickFreq(void);
 uint32_t HAL_GPIO_ReadPin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);
 uint32_t HAL_GPIO_WritePin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState);
 
-void SendPrechargeStatus();
-void SendDebugReport();
+void SendPrechargeStatus(void);
+void SendDebugReport(void);
 
 #endif
