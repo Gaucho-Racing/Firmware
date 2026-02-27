9ee1a724e2e10cd2ff19ad72911256bf6936a6d8
khoulihan27 <khoulihan@ucsb.edu>
2026-02-26 21:33:27 -0800

    lights done i think (BMS, IMD, BSPD)

--- ECU/Application/Src/Lights.c
+++ ECU/Application/Src/Lights.c
@@ -2,6 +2,7 @@
 
 #include "StateData.h"
 #include "StateUtils.h"
+#include "CANutils.h"
 #include "adc.h"
 #include "can.h"
 #include "main.h"
@@ -65,6 +66,32 @@ void SoftwareOKLightControl(ECU_StateData *stateLump)
 }
 
 // TODO: add BCU, IMS, and BSPD lights
+
+void BMSLights(ECU_StateData *stateLump) {
+	bool light = 0;
+	light |= stateLump->max_cell_temp_c > CRITICAL_MAX_CELL_TEMP_C;
+	light |= stateLump->ts_voltage > CRITICAL_TS_VOLTAGE;
+	light |= bmsFailure(stateLump);
+	// TODO: interrupted/missing BMS vals
+	GR_OLD_DASH_CONFIG_MSG message = {.led_bits = light};
+	ECU_CAN_Send(GR_OLD_BUS_PRIMARY, GR_DASH_PANEL, MSG_DASH_CONFIG, &message, sizeof(message));
+}
+
+void IMDLights(ECU_StateData *stateLump) {
+	uint8_t light = 0;
+	// TODO: isolation failure?
+	light |= imdFailure(stateLump);
+	GR_OLD_DASH_CONFIG_MSG message = {.led_bits = (light << 1)};
+	ECU_CAN_Send(GR_OLD_BUS_PRIMARY, GR_DASH_PANEL, MSG_DASH_CONFIG, &message, sizeof(message));
+}
+
+void BSPDLights(ECU_StateData *stateLump) {
+	uint8_t light = 0;
+	// TODO: isolation failure?
+	light |= bspdFailure(stateLump);
+	GR_OLD_DASH_CONFIG_MSG message = {.led_bits = (light << 2)};
+	ECU_CAN_Send(GR_OLD_BUS_PRIMARY, GR_DASH_PANEL, MSG_DASH_CONFIG, &message, sizeof(message));
+}
 
 void lightControl(ECU_StateData *stateData)
 {
