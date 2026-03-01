9d5a1180536de9482c983a8991c93504df1173c6
Daniel Hansen <dchansen06@gmail.com>
2026-02-28 18:20:44 -0800

    Use VCM instead of button input
    Signed-off-by: Daniel Hansen <dchansen06@gmail.com>

--- CCU/Core/Inc/main.h
+++ CCU/Core/Inc/main.h
@@ -36,6 +36,7 @@ extern "C" {
 #include "stm32g4xx_ll_exti.h"
 #include "stm32g4xx_ll_gpio.h"
 #include "stm32g4xx_ll_lpuart.h"
+#include "stm32g4xx_ll_usart.h"
 #include "stm32g4xx_ll_pwr.h"
 #include "stm32g4xx_ll_rcc.h"
 #include "stm32g4xx_ll_system.h"
