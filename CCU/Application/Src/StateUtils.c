4148ed2f40769f47ebccc3400904f4d2834223e4
khoulihan27 <khoulihan@ucsb.edu>
2026-03-01 22:19:26 -0800

    switched from HAL to LL for software latch function

--- CCU/Application/Src/StateUtils.c
+++ CCU/Application/Src/StateUtils.c
@@ -10,12 +10,12 @@
 void setSoftwareLatch(bool close, CCU_StateData *state_data)
 {
 
-	if (close && !(HAL_GPIO_ReadPin(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin))) {
-		HAL_GPIO_WritePin(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin, GPIO_PIN_SET);
+	if (close && !(LL_GPIO_IsInputPinSet(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin))) {
+		LL_GPIO_SetOutputPin(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin);
 		state_data->BCU_S2_SOFTWARE_LATCH = 1;
 		LOGOMATIC("Software Latch: High\n");
-	} else if (!(close) && HAL_GPIO_ReadPin(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin)) {
-		HAL_GPIO_WritePin(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin, GPIO_PIN_RESET);
+	} else if (!(close) && LL_GPIO_IsInputPinSet(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin)) {
+		LL_GPIO_SetOutputPin(SOFTWARE_OK_CONTROL_GPIO_Port, SOFTWARE_OK_CONTROL_Pin);
 		state_data->BCU_S2_SOFTWARE_LATCH = 0;
 		LOGOMATIC("Software Latch: Low\n");
 	}
