5f633aaa3f1aed4123ba5f777a1ced98e98f901a
Daniel Hansen <dchansen06@gmail.com>
2026-03-11 22:40:45 -0700

    VS Code crashing

---
+++ BLINKY/H5BLINKY/Core/Inc/stm32h5xx_hal_conf.h
@@ -0,0 +1,7 @@
+#ifndef STM32H5xx_HAL_CONF_H
+#define STM32H5xx_HAL_CONF_H
+
+// Not needed since we are not using the HAL library, but some of the generated code relies on it
+#include <stdint.h>
+
+#endif
---
+++ BLINKY/H5BLINKY/README.md
@@ -0,0 +1,5 @@
+# STM32H523 Blinky
+STM32H523CEU6
+
+Plug in the board following ST-LINK pinout
+Compile and flash H5BLINKY.elf
