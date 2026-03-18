df0c29791f77cb8fc331998148cd1074d97339d8
Daniel Hansen <dchansen06@gmail.com>
2026-03-17 22:31:28 -0700

    Mark binary flags as `__extension__` for pedantic rules
    Signed-off-by: Daniel Hansen <dchansen06@gmail.com>

--- Lib/GlobalShare/Inc/StateMachine.h
+++ Lib/GlobalShare/Inc/StateMachine.h
@@ -17,26 +17,26 @@ enum GR_ECU_State {
 	/**
 	 * The GLV is off and not operational (not possible if ECU is running)
 	 */
-	GR_GLV_OFF = 0b00000001,
+	GR_GLV_OFF = __extension__ 0b00000001,
 	/**
 	 * The ECU is on and ready for operation.
 	 */
-	GR_GLV_ON = 0b00000010,
+	GR_GLV_ON = __extension__ 0b00000010,
 	/**
 	 * The HV precharging process has been initiated.
 	 */
-	GR_PRECHARGE_ENGAGED = 0b00000100,
+	GR_PRECHARGE_ENGAGED = __extension__ 0b00000100,
 	/**
 	 * The HV precharging process has been completed successfully.
 	 */
-	GR_PRECHARGE_COMPLETE = 0b00001000,
+	GR_PRECHARGE_COMPLETE = __extension__ 0b00001000,
 	/**
 	 * The HV system is fully operational and the drive system is active.
 	 */
-	GR_DRIVE_ACTIVE = 0b00010000,
+	GR_DRIVE_ACTIVE = __extension__ 0b00010000,
 	/**
 	 * The HV system is in the process of discharging, TS Voltage >60V.
 	 */
-	GR_TS_DISCHARGE = 0b00100000,
+	GR_TS_DISCHARGE = __extension__ 0b00100000,
 };
 #endif
