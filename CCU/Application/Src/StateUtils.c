29d1af529463a3c9685cb6480b24337c06d73033
David Wilson <wilsondavid0611@gmail.com>
2026-03-31 22:03:53 -0700

    Initial SecondaryChecks function block

--- CCU/Application/Src/StateUtils.c
+++ CCU/Application/Src/StateUtils.c
@@ -65,3 +65,36 @@ bool CriticalError(const CCU_StateData *state_data)
 		return false;
 	}
 }
+
+
+bool SecondaryChecks(const CCU_StateData *state_data){
+
+	//TODO: Implement secondary checks based of rules (This may be a different file entirely)
+	//FIXME: Double check what that actually looks like and means
+
+	/** Check 1:
+	 * TS Voltage Discharge
+	 *
+	 * TS voltage must decrease below 60VDC in 5 sec.
+	 * Allow team to set pace of tests to prevent overheating precharge/discharge resistors.
+	 */
+
+
+
+
+	/** Check 2:
+	 * Maximum Power/Voltage Violation
+	 *
+	 *EV.3.4.1 A Violation occurs when one or two of these exist:
+		a. Use of more than the specified maximum power EV.3.3.1
+		b. Exceed the maximum voltage EV.3.3.2
+		for one or the two conditions:
+		• Continuously for 100 ms or more
+		• After a moving average over 500 ms is applied
+	 */
+
+
+
+
+
+}
