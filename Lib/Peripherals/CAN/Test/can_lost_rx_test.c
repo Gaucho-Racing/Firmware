f98dd17a39ab27df27d6c0e6900dfa71e5e5bfc5
Vihan Jayaraman <55373538+VihanJ@users.noreply.github.com>
2026-04-02 21:33:50 -0700

    fixed dlc in tests

--- Lib/Peripherals/CAN/Test/can_lost_rx_test.c
+++ Lib/Peripherals/CAN/Test/can_lost_rx_test.c
@@ -41,7 +41,7 @@ int can_lost_rx_test(void)
 	    .TxFrameType = FDCAN_DATA_FRAME,
 	    .ErrorStateIndicator = FDCAN_ESI_ACTIVE, // honestly this might be a value you have to read from a node
 						     // FDCAN_ESI_ACTIVE is just a state that assumes there are minimal errors
-	    .DataLength = 20,
+	    .DataLength = FDCAN_DLC_BYTES_20,
 	    .BitRateSwitch = FDCAN_BRS_OFF,
 	    .TxEventFifoControl = FDCAN_NO_TX_EVENTS, // change to FDCAN_STORE_TX_EVENTS if you need to store info regarding transmitted messages
 	    .MessageMarker = 0			      // also change this to a real address if you change fifo control
