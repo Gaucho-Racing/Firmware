77b570805f5d11fddcf6bc8f20cea5e31b7e774b
Daniel Hansen <dchansen06@gmail.com>
2026-04-05 23:20:39 -0700

    Formatting change because VS Code does not let me do edits and Ctrl+S without formatting, sorry about that but this does not change any code functionally...
    Signed-off-by: Daniel Hansen <dchansen06@gmail.com>

--- Lib/Peripherals/CAN/Src/can_dma.c
+++ Lib/Peripherals/CAN/Src/can_dma.c
@@ -1,17 +1,18 @@
 
-#include "stm32g4xx_ll_dma.h"
-#include "stm32g4xx_ll_bus.h"
-#include <stdbool.h>
-
 #include "can_dma.h"
 
-//#include "can_platform_deps.h"
-#include "STM32G4_hal_fdcan_defines.h"
+#include <stdbool.h>
+
+#include "stm32g4xx_ll_bus.h"
+#include "stm32g4xx_ll_dma.h"
+
+// #include "can_platform_deps.h"
 #include "Logomatic.h"
+#include "STM32G4_hal_fdcan_defines.h"
 
-#define DMA_INTERRUPT //can't even think about trying this yet
+#define DMA_INTERRUPT // can't even think about trying this yet
 
-//static void DMA_M2M_BlockingTransfer(uint8_t *src, uint8_t *dst, uint32_t byte_count);
+// static void DMA_M2M_BlockingTransfer(uint8_t *src, uint8_t *dst, uint32_t byte_count);
 static void DMA_M2M_Transfer(uint8_t *src, uint8_t *dst, uint32_t byte_count);
 
 /*
@@ -29,17 +30,18 @@ typedef struct dma_transfer_t {
 } dma_transfer_t;
 */
 
-//static volatile dma_transfer_t dma1_ch1 = {0};
+// static volatile dma_transfer_t dma1_ch1 = {0};
 
 static uint32_t msg_count = 0;
-HAL_StatusTypeDef FDCAN_GetRxMessage_DMA(FDCAN_HandleTypeDef* hfdcan, uint32_t RxLocation, FDCAN_RxHeaderTypeDef *pRxHeader, uint8_t *pRxData)
+HAL_StatusTypeDef FDCAN_GetRxMessage_DMA(FDCAN_HandleTypeDef *hfdcan, uint32_t RxLocation, FDCAN_RxHeaderTypeDef *pRxHeader, uint8_t *pRxData)
 {
 	uint32_t *RxAddress;
 	uint8_t *pData;
-	uint32_t ByteCounter; UNUSED(ByteCounter);
+	uint32_t ByteCounter;
+	UNUSED(ByteCounter);
 	uint32_t GetIndex;
 
-	//FDCAN_HandleTypeDef* hfdcan = handle->hal_fdcanP;
+	// FDCAN_HandleTypeDef* hfdcan = handle->hal_fdcanP;
 	HAL_FDCAN_StateTypeDef state = hfdcan->State;
 
 	/* Check function parameters */
@@ -98,8 +100,6 @@ HAL_StatusTypeDef FDCAN_GetRxMessage_DMA(FDCAN_HandleTypeDef* hfdcan, uint32_t R
 
 		msg_count++;
 
-
-
 		/* Retrieve IdType */
 		pRxHeader->IdType = *RxAddress & FDCAN_ELEMENT_MASK_XTD;
 
@@ -142,7 +142,6 @@ HAL_StatusTypeDef FDCAN_GetRxMessage_DMA(FDCAN_HandleTypeDef* hfdcan, uint32_t R
 		/* Increment RxAddress pointer to payload of Rx FIFO element */
 		RxAddress++;
 
-
 		/*LOGOMATIC("msg#%lu F0GI=%lu GetIndex=%lu RxFIFO0SA=0x%08lx PayloadAddr=0x%08lx\n",
 		msg_count,
 		(hfdcan->Instance->RXF0S & FDCAN_RXF0S_F0GI) >> FDCAN_RXF0S_F0GI_Pos,
@@ -158,12 +157,12 @@ HAL_StatusTypeDef FDCAN_GetRxMessage_DMA(FDCAN_HandleTypeDef* hfdcan, uint32_t R
 		/* Retrieve Rx payload with DMA*/
 		pData = (uint8_t *)RxAddress;
 		uint32_t bytes = DLCtoBytes[pRxHeader->DataLength];
-		//DMA_M2M_BlockingTransfer(pData, pRxData, bytes);
+// DMA_M2M_BlockingTransfer(pData, pRxData, bytes);
 
-
-		// ====================== THIS SECTION IS DIFFERENT FROM HAL_FDCAN_GetRxMessage()===========================
-		#ifdef DMA_INTERRUPT
-		while (dma1_ch1.unconsumed);
+// ====================== THIS SECTION IS DIFFERENT FROM HAL_FDCAN_GetRxMessage()===========================
+#ifdef DMA_INTERRUPT
+		while (dma1_ch1.unconsumed)
+			;
 
 		dma1_ch1.unconsumed = true;
 		dma1_ch1.can_id = pRxHeader->Identifier;
@@ -174,7 +173,7 @@ HAL_StatusTypeDef FDCAN_GetRxMessage_DMA(FDCAN_HandleTypeDef* hfdcan, uint32_t R
 
 		DMA_M2M_Transfer(pData, dma1_ch1.data, bytes);
 
-		#else
+#else
 		DMA_M2M_Transfer(pData, pRxData, bytes);
 
 		//===============================================================================
@@ -188,7 +187,7 @@ HAL_StatusTypeDef FDCAN_GetRxMessage_DMA(FDCAN_HandleTypeDef* hfdcan, uint32_t R
 			/* Acknowledge the Rx FIFO 1 that the oldest element is read so that it increments the GetIndex */
 			hfdcan->Instance->RXF1A = GetIndex;
 		}
-		#endif
+#endif
 
 		/*for (ByteCounter = 0; ByteCounter < DLCtoBytes[pRxHeader->DataLength]; ByteCounter++) {
 			pRxData[ByteCounter] = pData[ByteCounter];
@@ -196,7 +195,6 @@ HAL_StatusTypeDef FDCAN_GetRxMessage_DMA(FDCAN_HandleTypeDef* hfdcan, uint32_t R
 
 		/* Return function status */
 		return HAL_OK;
-
 
 	} else {
 		/* Update error code */
@@ -206,47 +204,41 @@ HAL_StatusTypeDef FDCAN_GetRxMessage_DMA(FDCAN_HandleTypeDef* hfdcan, uint32_t R
 	}
 }
 
-
-
 CAN_RXCallback DMA1_rx_callback = 0;
 void DMA_M2M_Init(uint32_t preempt, uint32_t subpriority, CAN_RXCallback callback)
 {
-    //Enable DMA1 clock
-    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_DMA1);
-    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_DMAMUX1);
+	// Enable DMA1 clock
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_DMA1);
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_DMAMUX1);
 
-    LL_DMA_ConfigTransfer(DMA1, LL_DMA_CHANNEL_1,
-        LL_DMA_DIRECTION_MEMORY_TO_MEMORY |
-        LL_DMA_MODE_NORMAL                |
-        LL_DMA_PERIPH_INCREMENT           |   // src increment
-        LL_DMA_MEMORY_INCREMENT           |   // dst increment
-        LL_DMA_PDATAALIGN_WORD            |   // src word (32-bit)
-        LL_DMA_MDATAALIGN_WORD            |   // dst word (32-bit)
-        LL_DMA_PRIORITY_HIGH);
+	LL_DMA_ConfigTransfer(DMA1, LL_DMA_CHANNEL_1,
+			      LL_DMA_DIRECTION_MEMORY_TO_MEMORY | LL_DMA_MODE_NORMAL | LL_DMA_PERIPH_INCREMENT | // src increment
+				  LL_DMA_MEMORY_INCREMENT |							 // dst increment
+				  LL_DMA_PDATAALIGN_WORD |							 // src word (32-bit)
+				  LL_DMA_MDATAALIGN_WORD |							 // dst word (32-bit)
+				  LL_DMA_PRIORITY_HIGH);
 
-    //For M2M, DMAMUX must be set to a software request line (0)
-    LL_DMA_SetPeriphRequest(DMA1, LL_DMA_CHANNEL_1, LL_DMAMUX_REQ_MEM2MEM);
+	// For M2M, DMAMUX must be set to a software request line (0)
+	LL_DMA_SetPeriphRequest(DMA1, LL_DMA_CHANNEL_1, LL_DMAMUX_REQ_MEM2MEM);
 
-	#ifndef DMA_INTERRUPT
+#ifndef DMA_INTERRUPT
 	UNUSED(preempt);
 	UNUSED(subpriority);
 	UNUSED(callback);
-	#else
+#else
 
 	DMA1_rx_callback = callback;
 
 	LL_DMA_ClearFlag_TC1(DMA1);
-    LL_DMA_ClearFlag_TE1(DMA1);
+	LL_DMA_ClearFlag_TE1(DMA1);
 
 	LL_DMA_EnableIT_TC(DMA1, LL_DMA_CHANNEL_1);
-    LL_DMA_EnableIT_TE(DMA1, LL_DMA_CHANNEL_1);
+	LL_DMA_EnableIT_TE(DMA1, LL_DMA_CHANNEL_1);
 
 	NVIC_SetPriority(DMA1_Channel1_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), preempt, subpriority));
-    NVIC_EnableIRQ(DMA1_Channel1_IRQn);
-	#endif
+	NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+#endif
 }
-
-
 
 /*
 #define RxFifoElementsNumber 3
@@ -270,41 +262,39 @@ bool is_valid_rxfifo1_address(FDCAN_HandleTypeDef *hfdcan, uint32_t *RxAddress) 
 	return false;
 }*/
 
-
 void DMA_M2M_Transfer(uint8_t *src, uint8_t *dst, uint32_t byte_count)
 {
 
-	if (byte_count == 0) return;
+	if (byte_count == 0) {
+		return;
+	}
 
-    /* Disable channel before reconfiguring */
-    LL_DMA_DisableChannel(DMA1, LL_DMA_CHANNEL_1);
+	/* Disable channel before reconfiguring */
+	LL_DMA_DisableChannel(DMA1, LL_DMA_CHANNEL_1);
 
-    LL_DMA_SetMemoryAddress(DMA1,  LL_DMA_CHANNEL_1, (uint32_t)dst);
+	LL_DMA_SetMemoryAddress(DMA1, LL_DMA_CHANNEL_1, (uint32_t)dst);
 
-	LL_DMA_SetPeriphAddress(DMA1,  LL_DMA_CHANNEL_1, (uint32_t)src);
-	//dma_src_start = src;
+	LL_DMA_SetPeriphAddress(DMA1, LL_DMA_CHANNEL_1, (uint32_t)src);
+	// dma_src_start = src;
 
-	//access FDDCAN Message RAM in 32-bit words, not in bytes
-	LL_DMA_SetDataLength(DMA1,     LL_DMA_CHANNEL_1, byte_count >> 2);
+	// access FDDCAN Message RAM in 32-bit words, not in bytes
+	LL_DMA_SetDataLength(DMA1, LL_DMA_CHANNEL_1, byte_count >> 2);
 
-	#ifdef DMA_INTERRUPT
-    /* Clear any pending flags before enabling */
-    LL_DMA_ClearFlag_TC1(DMA1);
-    LL_DMA_ClearFlag_TE1(DMA1);
-	#endif
+#ifdef DMA_INTERRUPT
+	/* Clear any pending flags before enabling */
+	LL_DMA_ClearFlag_TC1(DMA1);
+	LL_DMA_ClearFlag_TE1(DMA1);
+#endif
 
-    LL_DMA_EnableChannel(DMA1, LL_DMA_CHANNEL_1);
+	LL_DMA_EnableChannel(DMA1, LL_DMA_CHANNEL_1);
 
-	#ifndef DMA_INTERRUPT
-    /* Poll for transfer complete */
-    while (!LL_DMA_IsActiveFlag_TC1(DMA1)) {}
+#ifndef DMA_INTERRUPT
+	/* Poll for transfer complete */
+	while (!LL_DMA_IsActiveFlag_TC1(DMA1)) {}
 	__DSB();
 	LL_DMA_DisableChannel(DMA1, LL_DMA_CHANNEL_1);
-	#endif
+#endif
 }
-
-
-
 
 /*
 //TODO: Abstract out to handle other FDCAN instances besides FDCAN1 and FDCAN2
@@ -335,10 +325,10 @@ void DMA1_Channel1_IRQHandler(void)
 {
     if (LL_DMA_IsActiveFlag_TC1(DMA1))
     {
-        LL_DMA_ClearFlag_TC1(DMA1);
-        LL_DMA_DisableChannel(DMA1, LL_DMA_CHANNEL_1);
+	LL_DMA_ClearFlag_TC1(DMA1);
+	LL_DMA_DisableChannel(DMA1, LL_DMA_CHANNEL_1);
 
-        // transfer complete - notify application
+	// transfer complete - notify application
 		//FDCAN1_DMA_TC(dma_src_start);
 		if (dma1_ch1.RxLocation == FDCAN_RX_FIFO0) // Rx element is assigned to the Rx FIFO 0
 		{
@@ -358,10 +348,10 @@ void DMA1_Channel1_IRQHandler(void)
 
     if (LL_DMA_IsActiveFlag_TE1(DMA1))
     {
-        LL_DMA_ClearFlag_TE1(DMA1);
-        LL_DMA_DisableChannel(DMA1, LL_DMA_CHANNEL_1);
+	LL_DMA_ClearFlag_TE1(DMA1);
+	LL_DMA_DisableChannel(DMA1, LL_DMA_CHANNEL_1);
 
-        // handle error
+	// handle error
 		//TODO: Handle DMA Transfer error
 		transfer_errors++;
 
