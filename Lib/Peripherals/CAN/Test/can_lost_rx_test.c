4e593a48d498645e8fc7009383ca4352ad746908
Vihan Jayaraman <55373538+VihanJ@users.noreply.github.com>
2026-04-07 13:32:31 -0700

    CAN DMA Test (#395)
    # DMA Testing
    
    ## Problem and Scope
    
    Demonstrated POC for DMA transfers in Lib/Peripherals/CAN. Used Blocking DMA to speed up copies of received messages inside FDCAN message RAM, but did not exhibit a significant increase in performance compared to CPU copies. Time permitting (and only if necessary) can look into using DMA interrupts to optimize further. 
    
    ## Description
    
    
    ## Gotchas and Limitations
    
    
    ## Testing
    
    Run CAN_EXTERNAL_TEST in Lib/Peripherals/CAN/Test
    
    - [ ] HOOTL testing
    - [ ] HITL testing
    - [ ] Human tested
    
    ### Testing Details
    
    
    ## Larger Impact
    
    
    ## Additional Context and Ticket
    
    ---------
    
    Signed-off-by: Daniel Hansen <dchansen06@gmail.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Daniel Hansen <dchansen06@gmail.com>
    Co-authored-by: Daniel Hansen <105574022+dchansen06@users.noreply.github.com>

---
+++ Lib/Peripherals/CAN/Test/can_lost_rx_test.c
@@ -0,0 +1,128 @@
+#include <string.h>
+
+#include "can.h"
+#include "can_tests.h"
+
+// TODO:
+static volatile uint32_t can_rx_received = 0;
+void can_lost_rx_callback(uint32_t id, void *data, uint32_t size)
+{
+	can_rx_received++;
+	UNUSED(id);
+	UNUSED(data);
+	UNUSED(size);
+	return;
+}
+
+int can_lost_rx_test(void)
+{
+	LOGOMATIC("running can_lost_rx\n");
+	uint32_t status, loop;
+	UNUSED(status);
+
+	status = loop = 0;
+
+	CANHandle *primary_can, *data_can;
+	primary_can = data_can = NULL;
+	CANConfig cfg1;
+
+	if (get_cfg(FDCAN1, can_lost_rx_callback, &cfg1, FDCAN_MODE_INTERNAL_LOOPBACK, 0, 0)) {
+		LOGOMATIC("Could not get config for FDCAN1\n");
+		return ERROR;
+	}
+
+	can_set_clksource(LL_RCC_FDCAN_CLKSOURCE_PCLK1);
+
+	// TODO: Make the stress test more stressful
+	FDCAN_TxHeaderTypeDef TxHeader = {
+	    .Identifier = 1,
+
+	    .IdType = FDCAN_STANDARD_ID,
+	    .TxFrameType = FDCAN_DATA_FRAME,
+	    .ErrorStateIndicator = FDCAN_ESI_ACTIVE, // honestly this might be a value you have to read from a node
+						     // FDCAN_ESI_ACTIVE is just a state that assumes there are minimal errors
+	    .DataLength = FDCAN_DLC_BYTES_20,
+	    .BitRateSwitch = FDCAN_BRS_OFF,
+	    .TxEventFifoControl = FDCAN_NO_TX_EVENTS, // change to FDCAN_STORE_TX_EVENTS if you need to store info regarding transmitted messages
+	    .MessageMarker = 0			      // also change this to a real address if you change fifo control
+	};
+
+	if ((primary_can = can_init(&cfg1)) == NULL) {
+		LOGOMATIC("Could not initialize primary_can\n");
+		return ERROR;
+	}
+	if (can_start(primary_can)) {
+		LOGOMATIC("Could not start primary_can\n");
+		return ERROR;
+	}
+
+	FDCANTxMessage msg;
+	memset(&(msg.data), 0, sizeof(msg.data));
+	msg.data[0] = 0x80;
+	msg.tx_header = TxHeader;
+
+	size_t i = 0;
+	size_t rounds = 5;
+	size_t messages = primary_can->tx_capacity * 2;
+	uint32_t successes = 0;
+	uint32_t total_received = 0;
+	uint32_t total_sent = 0;
+	while (loop < rounds) {
+		loop++;
+		can_rx_received = 0;
+		i = 0;
+		while (i < messages) {
+			if (can_send(primary_can, &msg) != CAN_SUCCESS) {
+				LOGOMATIC("can_lost_rx: FAIL: sending CAN msg at %u-th consecutive send.\n", (unsigned int)i + 1);
+				break;
+			}
+			i++;
+		}
+		LOGOMATIC("Sent %u/%u CAN msgs...\n", (unsigned int)i, (unsigned int)messages);
+		HAL_Delay(1000);
+
+		LOGOMATIC("Received %u/%u CAN msgs after 1 second.\n", (unsigned int)can_rx_received, (unsigned int)messages);
+
+		total_sent += i;
+		total_received += can_rx_received;
+
+		LOGOMATIC("finished loop %ld\n", loop);
+
+		if (primary_can->tx_elements > 0) {
+			LOGOMATIC("can_lost_rx: FAIL: did not send all messages from tx_buffer\n");
+			continue;
+		}
+		LOGOMATIC("\n");
+
+		// check that all message were sent and received
+		if (can_rx_received == messages) {
+			successes += 1;
+		}
+		// msg.data[0] = 0x10;
+		// can_send(data_can, &msg);
+		// HAL_Delay(1000);
+	}
+
+	if (can_release(primary_can)) {
+		LOGOMATIC("can_lost_rx: FAIL: could not release primary_can\n");
+		return ERROR;
+	}
+
+	// FINAL CHECK
+	LOGOMATIC("can_lost_rx: succeeded %u/%u rounds\n", (unsigned int)successes, (unsigned int)rounds);
+	LOGOMATIC("can_lost_rx: instance logged %ld missed messages\n", primary_can->lost_rx);
+
+	if (primary_can->lost_rx + total_received != total_sent) {
+		LOGOMATIC("can_lost_rx: FAIL, did not log all missed messages\n");
+	} else {
+		LOGOMATIC("can_lost_rx: SUCCESS - logged all missed messages\n");
+	}
+
+	/*if (successes < rounds) {
+		LOGOMATIC("can_lost_rx: FAIL\n");
+	} else {
+		LOGOMATIC("can_lost_rx: SUCCESS\n");
+	}*/
+
+	return SUCCESS;
+}
---
+++ Lib/Peripherals/CAN/Test/profile.h
@@ -0,0 +1,143 @@
+#ifndef PROFILE_H
+#define PROFILE_H
+
+#define PROFILE 1
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "Logomatic.h"
+
+static inline uint32_t max_u32(uint32_t a, uint32_t b)
+{
+	return a > b ? a : b;
+}
+typedef struct dwt_timer_t {
+	uint32_t total_cycles;
+	uint32_t total_samples;
+
+	volatile uint32_t start_cycle;
+	volatile uint32_t end_cycle;
+
+	volatile uint32_t max_cycles;
+	volatile uint32_t min_cycles;
+
+	volatile bool measuring;
+} dwt_timer_t;
+
+static inline void start_dwt()
+{
+	// DWT->CYCCNT = 0;
+	CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+	DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+	__ISB();
+}
+
+static inline void stop_dwt()
+{
+	DWT->CTRL &= ~DWT_CTRL_CYCCNTENA_Msk;
+}
+
+// static number of cycles is 37
+#define STATIC_CYCLE_COST 36
+
+static inline float dwt_timer_average_cycles(const dwt_timer_t *dt)
+{
+	return (dt->total_cycles) / ((float)dt->total_samples);
+}
+static inline void dwt_timer_start_measurement(dwt_timer_t *dt)
+{
+	dt->measuring = true;
+	dt->start_cycle = DWT->CYCCNT;
+	__DSB();
+	__ISB();
+}
+
+static inline void dwt_timer_end_measurement(dwt_timer_t *dt)
+{
+	if (!dt->measuring) {
+		return;
+	}
+
+	dt->end_cycle = DWT->CYCCNT;
+
+	// handles one overflow
+	uint32_t elapsed_cycle = dt->end_cycle - dt->start_cycle;
+
+	dt->total_cycles += elapsed_cycle; //- STATIC_CYCLE_COST;
+	dt->total_samples++;
+
+	dt->measuring = false;
+
+	dt->max_cycles = max_u32(dt->max_cycles, elapsed_cycle);
+	__DSB();
+}
+
+static inline void dwt_timer_reset(dwt_timer_t *dt)
+{
+	dwt_timer_end_measurement(dt);
+
+	dt->start_cycle = 0;
+	dt->end_cycle = 0;
+	dt->measuring = false;
+	dt->total_cycles = 0;
+	dt->total_samples = 0;
+}
+
+static inline void dwt_timer_print_info(dwt_timer_t *dt)
+{
+	LOGOMATIC("AVG: %f\n", dwt_timer_average_cycles(dt));
+	LOGOMATIC("Total Cycles: %lu\n", dt->total_cycles);
+	LOGOMATIC("Total Samples: %lu\n", dt->total_samples);
+	LOGOMATIC("Max Cycle: %lu\n", dt->max_cycles);
+}
+
+// testing
+extern void profiler_test();
+
+/*extern dwt_timer_t GLOBAL_DWT_TIMER;
+
+//static number of cycles is 30
+static inline void global_dwt_timer_start_measurement(void) {
+    GLOBAL_DWT_TIMER.measuring = true;
+    GLOBAL_DWT_TIMER.start_cycle = DWT->CYCCNT;
+}
+
+static inline void global_dwt_timer_end_measurement(void) {
+    if (!GLOBAL_DWT_TIMER.measuring) {
+	return;
+    }
+
+    GLOBAL_DWT_TIMER.end_cycle = DWT->CYCCNT;
+
+    //handles one overflow
+    uint32_t elapsed_cycle = GLOBAL_DWT_TIMER.end_cycle - GLOBAL_DWT_TIMER.start_cycle;
+    GLOBAL_DWT_TIMER.total_cycles += elapsed_cycle;
+    GLOBAL_DWT_TIMER.total_samples++;
+
+    GLOBAL_DWT_TIMER.measuring = false;
+
+    GLOBAL_DWT_TIMER.max_cycles = MAX(GLOBAL_DWT_TIMER.max_cycles, elapsed_cycle);
+    //GLOBAL_DWT_TIMER.min_cycles;
+}
+static inline float global_dwt_timer_average_cycles() {
+    return (GLOBAL_DWT_TIMER.total_cycles)/((float) GLOBAL_DWT_TIMER.total_samples);
+
+}*/
+
+// extern volatile uint32_t PROFILE_AVG_RX_CYCLES;
+// extern volatile uint32_t PROFILE_RX_SAMPLES;
+// extern volatile uint32_t ELAPSED_CYCLES;
+// extern volatile float AVG;
+
+/*#define START_COUNT \
+	do { \
+	    DWT->CYCCNT = 0; \
+	    DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk; \
+	} while ();
+
+#define END_COUNT
+#define PROFILER_STOP (DWT->CTRL &= ~DWT_CTRL_CYCCNTENA_Msk;)
+*/
+
+#endif
