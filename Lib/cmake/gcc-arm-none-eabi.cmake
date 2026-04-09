9b12f8fc049725db4362af8a8f573a31ad7d9a64
Daniel Hansen <dchansen06@gmail.com>
2026-04-08 22:55:39 -0700

    IT IS RUN TWICE! Cannot be self-referential, verified that prior has no flags so it is safe to do as a one off
    Signed-off-by: Daniel Hansen <dchansen06@gmail.com>

--- Lib/cmake/gcc-arm-none-eabi.cmake
+++ Lib/cmake/gcc-arm-none-eabi.cmake
@@ -20,37 +20,37 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 # Linker, Compiler, and Assembler Flags
 set(
 	CMAKE_C_LINK_FLAGS
-	"${CMAKE_C_LINK_FLAGS} ${TARGET_FLAGS} --specs=nano.specs -Wl,--gc-sections -Wl,--start-group -lc -lm -Wl,--end-group -Wl,--print-memory-usage"
+	"${TARGET_FLAGS} --specs=nano.specs -Wl,--gc-sections -Wl,--start-group -lc -lm -Wl,--end-group -Wl,--print-memory-usage"
 )
 set(
 	CMAKE_C_FLAGS
-	"${CMAKE_C_FLAGS} ${TARGET_FLAGS} -Wall -Wextra -Wpedantic -fdata-sections -ffunction-sections -fstack-usage"
+	"${TARGET_FLAGS} -Wall -Wextra -Wpedantic -fdata-sections -ffunction-sections -fstack-usage"
 )
 set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} -x assembler-with-cpp -MMD -MP")
 
 # Compiler Flags for Build Types
-set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -O0 -g3")
+set(CMAKE_C_FLAGS_DEBUG "-O0 -g3")
 set(
 	CMAKE_C_FLAGS_RELWITHDEBINFO
-	"${CMAKE_C_FLAGS_RELWITHDEBINFO} -Og -g3 -Werror"
+	"-Og -g3 -Werror"
 )
-set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O2 -g0 -Werror -flto=auto")
+set(CMAKE_C_FLAGS_RELEASE "-O2 -g0 -Werror -flto=auto")
 set(
 	CMAKE_C_FLAGS_MINSIZEREL
-	"${CMAKE_C_FLAGS_MINSIZEREL} -Os -g0 -Werror -flto=auto"
+	"-Os -g0 -Werror -flto=auto"
 )
 
 # Linker Flags for Build Types
-set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS_DEBUG} -fno-lto")
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG "-fno-lto")
 set(
 	CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO
-	"${CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO} -Wl,--fatal-warnings"
+	"-Wl,--fatal-warnings"
 )
 set(
 	CMAKE_EXE_LINKER_FLAGS_RELEASE
-	"${CMAKE_EXE_LINKER_FLAGS_RELEASE} -Wl,--fatal-warnings -flto=auto"
+	"-Wl,--fatal-warnings -flto=auto"
 )
 set(
 	CMAKE_EXE_LINKER_FLAGS_MINSIZEREL
-	"${CMAKE_EXE_LINKER_FLAGS_MINSIZEREL} -Wl,--fatal-warnings -flto=auto"
+	"-Wl,--fatal-warnings -flto=auto"
 )
