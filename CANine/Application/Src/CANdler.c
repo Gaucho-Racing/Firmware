f52de54565b4d5dcfccad033d0cb15d694ef98d3
Nandini <nandinim.dharwadkar@gmail.com>
2026-04-09 21:06:11 -0700

    so like idk what to do but we writing switch cases for message handling ig

---
+++ CANine/Application/Src/CANdler.c
@@ -0,0 +1,18 @@
+#include "CANdler.h"
+
+#include <stdint.h>
+
+#include "GRCAN_BUS_ID.h"
+#include "GRCAN_MSG_ID.h"
+#include "GRCAN_NODE_ID.h"
+#include "GRCAN_MSG_DATA.h"
+#include "Logomatic.h"
+#include "Pinging.h"
+#include "StateData.h"
+#include "bitManipulations.h"
+
+
+
+void ECU_CAN_MessageHandler(GRCAN_BUS_ID bus_id, GRCAN_MSG_ID msg_id, GRCAN_NODE_ID sender_id, uint8_t *data, uint32_t data_length){
+
+}
