d14934825f6b93d9aa6b8ef8dfd5f4b582901e54
Shravya Salem Sathish <shravyassathish@gmail.com>
2026-04-13 14:47:10 -0700

    Changed HSE, LSE, CSI clock speeds to match H5

--- SAMM/SAMM_Mag/Inc/stm32h5xx_hal_conf.h
+++ SAMM/SAMM_Mag/Inc/stm32h5xx_hal_conf.h
@@ -115,12 +115,21 @@ extern "C" {
  *        (when HSE is used as system clock source, directly or through the PLL).
  */
 #if !defined(HSE_VALUE)
-#define HSE_VALUE (8000000UL) /*!< Value of the External oscillator in Hz */
+#define HSE_VALUE (25000000UL) /*!< Value of the External oscillator in Hz */
 #endif			      /* HSE_VALUE */
 
 #if !defined(HSE_STARTUP_TIMEOUT)
 #define HSE_STARTUP_TIMEOUT (100UL) /*!< Time out for HSE start up, in ms */
 #endif				    /* HSE_STARTUP_TIMEOUT */
+
+
+/**
+  * @brief Internal Core Speed oscillator (CSI) default value.
+  *        This value is the default CSI range value after Reset.
+  */
+#if !defined  (CSI_VALUE)
+  #define CSI_VALUE (4000000UL) /*!< Value of the Internal oscillator in Hz*/
+#endif /* CSI_VALUE */
 
 /**
  * @brief Internal High Speed oscillator (HSI) value.
@@ -128,7 +137,7 @@ extern "C" {
  *        (when HSI is used as system clock source, directly or through the PLL).
  */
 #if !defined(HSI_VALUE)
-#define HSI_VALUE (16000000UL) /*!< Value of the Internal oscillator in Hz*/
+#define HSI_VALUE (64000000UL) /*!< Value of the Internal oscillator in Hz*/
 #endif			       /* HSI_VALUE */
 
 /**
