fea9db8f8c0659cd941654237812bd71c442f049
Daniel Hansen <dchansen06@gmail.com>
2026-04-22 00:36:05 -0700

    Unify notification strategy
    Signed-off-by: Daniel Hansen <dchansen06@gmail.com>

--- TireTemp/GRCAN/NodeID.h
+++ TireTemp/GRCAN/NodeID.h
@@ -7,10 +7,10 @@
 #ifndef NODE
 #pragma message("NODE is not defined. Please compile Tire Temp for a specific target. Defaulting to FL.")
 #define LOCAL_GR_ID GRCAN_TireTemp_FL
-#pragma message "Compiling Tire Temp node as " STRINGIFY_MACRO(LOCAL_GR_ID)
 #else
 #define LOCAL_GR_ID CONCAT(GRCAN_TireTemp_, NODE)
-#pragma message "Compiling Tire Temp node as " STRINGIFY_MACRO(LOCAL_GR_ID)
 #endif
+
+#pragma message "Compiling Tire Temp node as " STRINGIFY_MACRO(LOCAL_GR_ID)
 
 #endif
