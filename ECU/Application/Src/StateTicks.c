68e6122527be2f8bb0b87d2ebb586eda4dd10cf5
gr-jetson <team@gauchoracing.com>
2026-05-09 19:58:58 -0700

    red light start up

--- ECU/Application/Src/StateTicks.c
+++ ECU/Application/Src/StateTicks.c
@@ -38,7 +38,9 @@ void ECU_State_Tick(void)
 		last_ECU_status_msg_millis = millis_since_boot;
 	}
 
-	stateLump.tssi_fault = bmsFailure(&stateLump) || imdFailure(&stateLump);
+	stateLump.tssi_fault = (stateLump.ams_sense < 0.5f ) || (stateLump.imd_sense < 0.5f);
+
+	// bmsFailure(&stateLump) || imdFailure(&stateLump);
 
 	switch (stateLump.ecu_state) {
 		case GR_GLV_OFF:
