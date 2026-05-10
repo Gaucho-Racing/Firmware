48cc6c5dca0ffa85e3f5aa247a2ce4b5ca7912e7
Daniel Hansen <dchansen06@gmail.com>
2026-05-09 18:07:15 -0700

    MacOS test is annoying
    Signed-off-by: Daniel Hansen <dchansen06@gmail.com>

--- Lib/Utils/Logomatic/logomatic.cmake
+++ Lib/Utils/Logomatic/logomatic.cmake
@@ -1,33 +1,21 @@
 add_library(LOGOMATIC_LIB INTERFACE)
 
+# Include directories and source files for the logomatic library
 target_include_directories(
 	LOGOMATIC_LIB
 	INTERFACE
 		${CMAKE_CURRENT_LIST_DIR}/Inc
 )
-
 target_sources(
 	LOGOMATIC_LIB
 	INTERFACE
 		${CMAKE_CURRENT_LIST_DIR}/Src/Logomatic.c
 )
-
 target_link_libraries(LOGOMATIC_LIB INTERFACE GLOBALSHARE_LIB)
 
+# Main compilation flag to enable logomatic
 if(CMAKE_LOGOMATIC_ENABLED)
 	target_compile_definitions(LOGOMATIC_LIB INTERFACE LOGOMATIC_ENABLED)
-	target_compile_options(
-		LOGOMATIC_LIB
-		INTERFACE
-			-u
-			_printf_float
-	)
-	target_link_options(
-		LOGOMATIC_LIB
-		INTERFACE
-			-u
-			_printf_float
-	)
 endif()
 
 if(CMAKE_PRESET_NAME STREQUAL "HOOTLTest")
@@ -58,4 +46,18 @@ if(CMAKE_PRESET_NAME STREQUAL "HOOTLTest")
 	)
 	target_link_libraries(logomatic_float PRIVATE LOGOMATIC_LIB)
 	add_test(logomatic_float_test logomatic_float)
+else()
+	# Add floating point support for printf, which goes unused on MacOS hootl builds and throws a warning
+	target_compile_options(
+		LOGOMATIC_LIB
+		INTERFACE
+			-u
+			_printf_float
+	)
+	target_link_options(
+		LOGOMATIC_LIB
+		INTERFACE
+			-u
+			_printf_float
+	)
 endif()
