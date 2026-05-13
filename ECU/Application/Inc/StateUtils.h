5d63c9d7d756e37b067802b577f998abd2560516
Daniel Hansen <dchansen06@gmail.com>
2026-05-13 06:22:27 -0700

    Random tested values for throttle
    Signed-off-by: Daniel Hansen <dchansen06@gmail.com>

--- ECU/Application/Inc/StateUtils.h
+++ ECU/Application/Inc/StateUtils.h
@@ -15,10 +15,10 @@ uint32_t MillisecondsSinceBoot(void);
 #define BRAKE_F_MAX 4095    // TODO: need to be determined FIXME: Rename better
 #define BRAKE_R_MIN 0	    // TODO: need to be determined FIXME: Rename better
 #define BRAKE_R_MAX 4095    // TODO: need to be determined FIXME: Rename better
-#define THROTTLE_MIN_1 0    // TODO: need to be determined
-#define THROTTLE_MAX_1 4095 // TODO: need to be determined
-#define THROTTLE_MIN_2 0    // TODO: need to be determined
-#define THROTTLE_MAX_2 4095 // TODO: need to be determined
+#define THROTTLE_MIN_1 308    // TODO: need to be determined
+#define THROTTLE_MAX_1 3689 // TODO: need to be determined
+#define THROTTLE_MIN_2 303    // TODO: need to be determined
+#define THROTTLE_MAX_2 3735 // TODO: need to be determined
 #define BSE_MAX 4096.0f
 #define BSE_DEADZONE 1.2f
 #define MAX_BSE_FAILURE_TIME 100
