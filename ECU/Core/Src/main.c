0e921c2e0d74a4db2d491f740cdba7a156d8ff24
Casey Zwicker <caseypzwicker@gmail.com>
2026-05-21 18:33:43 -0700

    readded MX DMA

--- ECU/Core/Src/main.c
+++ ECU/Core/Src/main.c
@@ -382,7 +382,7 @@ int main(void)
 	/* Initialize all configured peripherals */
 	// TODO: Figure out GPIO Init
 	MX_GPIO_Init();
-	// MX_DMA_Init();
+	MX_DMA_Init();
 	// MX_FDCAN1_Init();
 	MX_ADC1_Init();
 	MX_ADC2_Init();
