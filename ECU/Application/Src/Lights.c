5949b128880ba776cd6694d45e0a9b31d5fb1971
kzwicker <caseypzwicker@gmail.com>
2026-05-21 23:46:48 -0700

    RTD Button in "GLV On" Cycles Power Level (#491)
    # RTD Cycle Power Level
    
    ## Problem and Scope
    Want configurable power level
    
    ## Description
    Cycle the power level when pressing RTD while in GLV_ON. BSPD light
    blinks to indicate level.
    
    4 blinks: max powah
    3 blinks: half powah
    2 blinks: quarter powah
    1 blink: eighth powah
    
    Defaults to max, only blinks in GLV_ON
    
    ## Gotchas and Limitations
    Needs testing
    
    ## Testing
    
    - [x] HOOTL testing
    - [ ] HITL testing
    - [ ] Human tested
    
    ### Testing Details
    HOOTL only
    
    ## Larger Impact
    Driver experience
    
    ## Additional Context and Ticket
    @rnguyen144 requested
    
    ---------
    
    Signed-off-by: Daniel Hansen <105574022+dchansen06@users.noreply.github.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Daniel Hansen <105574022+dchansen06@users.noreply.github.com>

--- ECU/Application/Src/Lights.c
+++ ECU/Application/Src/Lights.c
@@ -55,11 +55,14 @@ void TSActiveButtonLightControl(ECU_StateData *stateLump)
 
 void dashLights(ECU_StateData *stateLump)
 {
+	uint8_t timeState = (MillisecondsSinceBoot() >> 8) % 16;						      // counter from 0 to 15 that increments every 256 ms:
+	bool powerLevelLight = (stateLump->ecu_state == GR_GLV_ON) && (timeState == (stateLump->powerlevel + 1) * 2); // 2 Hz light: # of blinks is power level plus one
+
 	// light control for if signal goog
-	GRCAN_DASH_CONFIG_MSG message = {.led_latch_flags = bspdFailure(stateLump) << 2 | stateLump->imd_light << 1 | stateLump->bms_light};
+	GRCAN_DASH_CONFIG_MSG message = {.led_latch_flags = (bspdFailure(stateLump) || powerLevelLight) << 2 | stateLump->imd_light << 1 | stateLump->bms_light};
 
 	// this is needed for the latch open control
-	message.led_latch_flags |= ((uint8_t)!bspdFailure(stateLump) << 5) | ((uint8_t)!stateLump->imd_light << 4) | ((uint8_t)!stateLump->bms_light << 3);
+	message.led_latch_flags |= ((uint8_t) !(bspdFailure(stateLump) || powerLevelLight) << 5) | ((uint8_t)!stateLump->imd_light << 4) | ((uint8_t)!stateLump->bms_light << 3);
 
 	ECU_CAN_Send(GRCAN_BUS_PRIMARY, GRCAN_Dash_Panel, GRCAN_DASH_CONFIG, &message, sizeof(message));
 }
