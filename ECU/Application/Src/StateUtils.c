f1153c9b42ab3d7749c37d903e21a392c8439008
Daniel Hansen <105574022+dchansen06@users.noreply.github.com>
2026-06-04 20:02:47 -0700

    Remove BSE unused comment
    Signed-off-by: Daniel Hansen <105574022+dchansen06@users.noreply.github.com>

--- ECU/Application/Src/StateUtils.c
+++ ECU/Application/Src/StateUtils.c
@@ -140,7 +140,7 @@ bool PressingBrake(volatile const ECU_StateData *stateData)
 	}
 #endif
 
-	// BSE unused
+
 	return (stateData->Brake_F_Signal > stateData->brake_f_min) || (stateData->Brake_R_Signal > stateData->brake_r_min);
 }
 
@@ -150,7 +150,6 @@ float CalcBrakePressure(volatile const ECU_StateData *stateData)
 	return 0;
 #endif
 
-	// BSE unused
 	float psi_front = stateData->Brake_F_Signal / 4096.0f * 5000.0f;
 	float psi_rear = stateData->Brake_R_Signal / 4096.0f * 5000.0f;
 	return fmaxf(psi_front, psi_rear);
