fc46fb8880f78af0d6ccc9560f0146b16a9215f2
Daniel Hansen <dchansen06@gmail.com>
2026-06-06 13:58:10 -0700

    Ensure that raw send capability is exposed by the underlying grcan fancy layer
    Signed-off-by: Daniel Hansen <dchansen06@gmail.com>

--- Lib/FancyLayers-RENAME/GRCAN/Inc/GRCAN_FancyLayer.h
+++ Lib/FancyLayers-RENAME/GRCAN/Inc/GRCAN_FancyLayer.h
@@ -28,5 +28,6 @@ uint32_t GRCAN_Fancy_EncodeID(GRCAN_Fancy_ID *id);
 void GRCAN_Fancy_DecodeID(GRCAN_Fancy_ID *id, uint32_t rawID);
 
 bool GRCAN_Fancy_Send(GRCAN_BUS_ID bus, GRCAN_NODE_ID destNode, GRCAN_MSG_ID messageID, void *data, uint32_t size);
+bool GRCAN_Raw_Send(GRCAN_BUS_ID bus, uint32_t rawID, void *data, uint32_t size);
 
 #endif
