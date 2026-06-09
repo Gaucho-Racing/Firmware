e04322a43723ee0248e0af588179056a6ecd151e
Daniel Hansen <dchansen06@gmail.com>
2026-06-09 10:53:09 -0700

    Move driving critical decisions from brake f and brake r to just bse/f
    https://discord.com/channels/756738476887638107/1457989718741618788/1513853014874521680
    Signed-off-by: Daniel Hansen <dchansen06@gmail.com>

--- ECU/Application/Src/StateUtils.c
+++ ECU/Application/Src/StateUtils.c
@@ -140,7 +140,7 @@ bool PressingBrake(volatile const ECU_StateData *stateData)
 	}
 #endif
 
-	return (stateData->Brake_F_Signal > stateData->brake_f_min) || (stateData->Brake_R_Signal > stateData->brake_r_min);
+	return stateData->bse_signal > stateData->brake_bse_min;
 }
 
 float CalcBrakePressure(volatile const ECU_StateData *stateData)
@@ -149,9 +149,7 @@ float CalcBrakePressure(volatile const ECU_StateData *stateData)
 	return 0;
 #endif
 
-	float psi_front = stateData->Brake_F_Signal / 4096.0f * 5000.0f;
-	float psi_rear = stateData->Brake_R_Signal / 4096.0f * 5000.0f;
-	return fmaxf(psi_front, psi_rear);
+	return stateData->bse_signal / 4096.0f * 5000.0f;
 }
 
 // TODO: reconsider deadzone
