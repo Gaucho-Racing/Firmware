c512dfe6607f7abe1ed638616923214821cf33a5
Daniel Hansen <dchansen06@gmail.com>
2026-08-23 16:05:16 -0700

    Adding a project (format)
    Signed-off-by: Daniel Hansen <dchansen06@gmail.com>

--- Lib/Vendor/TargetFlags/stm32h5xx.cmake
+++ Lib/Vendor/TargetFlags/stm32h5xx.cmake
@@ -1,8 +1,15 @@
 target_compile_options(
-	${TARGET_NAME}
-	PRIVATE
-		-mcpu=cortex-m33
-		-mfpu=fpv5-sp-d16
-		-mfloat-abi=hard
-		-mthumb
+    "${TARGET_NAME}"
+    PRIVATE
+    -mcpu=cortex-m33
+    -mfpu=fpv5-sp-d16
+    -mfloat-abi=hard
+)
+
+target_link_options(
+    "${TARGET_NAME}"
+    PRIVATE
+    -mcpu=cortex-m33
+    -mfpu=fpv5-sp-d16
+    -mfloat-abi=hard
 )
