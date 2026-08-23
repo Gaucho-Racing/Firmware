bed5350c25ba283f1cc357c4fca34dffd0573ebb
Daniel Hansen <dchansen06@gmail.com>
2026-08-23 16:49:49 -0700

    Add G4Hello (format)
    Signed-off-by: Daniel Hansen <dchansen06@gmail.com>

--- Testbench/testbench.cmake
+++ Testbench/testbench.cmake
@@ -17,3 +17,13 @@ add_project(
     CMSIS_GIT_TAG "v1.2.6" # https://github.com/STMicroelectronics/cmsis-device-g4/tree/v1.2.6
     CMSIS_MAJOR "5"
 )
+
+add_project(
+    NAME "NucleoG4-Hello"
+    PATH "NucleoG4-Hello"
+    SERIES "g4"
+    SPECIFIER "g474re"
+    HAL_GIT_TAG "v1.2.7" # https://github.com/STMicroelectronics/stm32g4xx-hal-driver/tree/v1.2.7
+    CMSIS_GIT_TAG "v1.2.6" # https://github.com/STMicroelectronics/cmsis-device-g4/tree/v1.2.6
+    CMSIS_MAJOR "5"
+)
